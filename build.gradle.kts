b840e8abd6175ae2caf6a33255f928771ccb4265
Aziza <aziza.helmy35@gmail.com>
2023-06-02 00:17:31 +0300

    finish modularization 🎉

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@
 plugins {
     id ("com.android.application") version "8.0.0" apply false
     id ("com.android.library") version "8.0.0" apply false
-    id ("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    kotlin("android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
 }
---
+++ user/domain/src/main/java/org/the_chance/domain/MyClass.kt
@@ -0,0 +1,3 @@
+package org.the_chance.domain
+
+class MyClass
