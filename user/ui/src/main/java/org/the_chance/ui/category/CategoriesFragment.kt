ea0b758b67d8c24e61f0cb25f42bbc9f89e5676e
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-06-03 01:18:20 +0300

    add dataBinding

--- user/ui/src/main/java/org/the_chance/ui/category/CategoriesFragment.kt
+++ user/ui/src/main/java/org/the_chance/ui/category/CategoriesFragment.kt
@@ -1,22 +1,13 @@
 package org.the_chance.ui.category
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModel
 import org.the_chance.ui.BaseFragment
 import org.the_chance.ui.R
+import org.the_chance.ui.databinding.FragmentCategoriesBinding
 
 class CategoriesFragment : BaseFragment<FragmentCategoriesBinding>()  {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
     override val TAG: String = this::class.simpleName.toString()
-
     override val layoutIdFragment: Int
         get() = R.layout.fragment_categories
     override val viewModel: ViewModel
