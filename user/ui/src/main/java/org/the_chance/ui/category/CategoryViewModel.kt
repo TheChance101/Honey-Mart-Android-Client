62f7dca5fe072f8c22c1dc1c4bfd78c0dbf273df
Asia sama <sehunexo710@gmail.com>
2023-06-03 14:25:33 +0300

    add missing annotations

--- user/ui/src/main/java/org/the_chance/ui/category/CategoryViewModel.kt
+++ user/ui/src/main/java/org/the_chance/ui/category/CategoryViewModel.kt
@@ -1,7 +1,8 @@
 package org.the_chance.ui.category
 
+import dagger.hilt.android.lifecycle.HiltViewModel
 import org.the_chance.ui.BaseViewModel
-
+@HiltViewModel
 class CategoryViewModel : BaseViewModel() {
     override val TAG: String = this::class.java.simpleName
 }
