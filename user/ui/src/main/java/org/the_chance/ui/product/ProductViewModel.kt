69739e7bee671142146916178d9138a58443cbbc
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-06-03 15:23:16 +0300

    initiate CategoryProductAdapter

--- user/ui/src/main/java/org/the_chance/ui/product/ProductViewModel.kt
+++ user/ui/src/main/java/org/the_chance/ui/product/ProductViewModel.kt
@@ -4,6 +4,11 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import org.the_chance.ui.BaseViewModel
 
 @HiltViewModel
-class ProductViewModel : BaseViewModel() {
+class ProductViewModel : BaseViewModel(), CategoryProductInteractionListener {
     override val TAG: String = this::class.java.simpleName
+
+
+    override fun onClickCategoryProduct(id: Int) {
+        TODO("Not yet implemented")
+    }
 }
--- user/ui/src/main/java/org/the_chance/ui/product/ProductsFragment.kt
+++ user/ui/src/main/java/org/the_chance/ui/product/ProductsFragment.kt
@@ -1,7 +1,6 @@
 package org.the_chance.ui.product
 
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.ui.BaseFragment
 import org.the_chance.ui.R
@@ -12,4 +11,14 @@ class ProductsFragment : BaseFragment<FragmentProductsBinding>() {
     override val TAG: String = this::class.simpleName.toString()
     override val layoutIdFragment = R.layout.fragment_products
     override val viewModel: ProductViewModel by viewModels()
+
+    override fun setup() {
+        initiateAdapter()
+    }
+ 
+ private fun initiateAdapter(){
+     val adapter = CategoryProductAdapter(viewModel)
+     binding.recyclerCategoryProduct.adapter = adapter
+ }
+
 }
