89b32ff8e3772f3ad702dbae96d15918169d249d
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-03 19:24:30 +0300

    add class UiState to adapters

--- user/ui/src/main/java/org/the_chance/ui/product/CategoryProductAdapter.kt
+++ user/ui/src/main/java/org/the_chance/ui/product/CategoryProductAdapter.kt
@@ -3,9 +3,10 @@ package org.the_chance.ui.product
 import org.the_chance.ui.BaseAdapter
 import org.the_chance.ui.BaseInteractionListener
 import org.the_chance.ui.R
+import org.the_chance.ui.product.uistste.ProductUiState
 
 class CategoryProductAdapter(listener: CategoryProductInteractionListener) :
-    BaseAdapter<ProductDto>(listener){
+    BaseAdapter<ProductUiState>(listener) {
     override val layoutID: Int = R.layout.item_category_product
 }
 
--- user/ui/src/main/java/org/the_chance/ui/product/ProductAdapter.kt
+++ user/ui/src/main/java/org/the_chance/ui/product/ProductAdapter.kt
@@ -3,8 +3,9 @@ package org.the_chance.ui.product
 import org.the_chance.ui.BaseAdapter
 import org.the_chance.ui.BaseInteractionListener
 import org.the_chance.ui.R
+import org.the_chance.ui.product.uistste.ProductUiState
 
-class ProductAdapter(listener: ProductInteractionListener) : BaseAdapter<ProductDto>(listener){
+class ProductAdapter(listener: ProductInteractionListener) : BaseAdapter<ProductUiState>(listener){
     override val layoutID: Int = R.layout.item_products
 }
 
