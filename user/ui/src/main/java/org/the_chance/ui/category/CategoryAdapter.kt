6387434cc8cf96d7b2361d44ea58950ce47e7b3b
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-03 16:47:23 +0300

    implement CategoryAdapter

--- user/ui/src/main/java/org/the_chance/ui/category/CategoryAdapter.kt
+++ user/ui/src/main/java/org/the_chance/ui/category/CategoryAdapter.kt
@@ -1,4 +1,13 @@
 package org.the_chance.ui.category
 
-class CategoryAdapter {
+import org.the_chance.ui.BaseAdapter
+import org.the_chance.ui.BaseInteractionListener
+import org.the_chance.ui.R
+
+class CategoryAdapter(
+    listener: CategoryInteractionListener
+) : BaseAdapter<CategoryUiState>(listener) {
+    override val layoutID = R.layout.item_category
 }
+
+interface CategoryInteractionListener : BaseInteractionListener
