cabd587c6961e5fee3cc6252929938ff1deec374
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-03 18:30:37 +0300

    implement CategoryUiState

--- user/ui/src/main/java/org/the_chance/ui/category/CategoryUiState.kt
+++ user/ui/src/main/java/org/the_chance/ui/category/CategoryUiState.kt
@@ -1,3 +1,7 @@
 package org.the_chance.ui.category
 
-data class CategoryUiState()
+data class CategoryUiState(
+    val categoryId: Long = 0L,
+    val categoryImageId: Int = 0,
+    val CategoryName: String = ""
+)
