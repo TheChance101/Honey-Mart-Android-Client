d85ef204b975e1199637911e8114d38e26d6cd09
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-06-03 15:52:26 +0300

    create product adapter

--- user/ui/src/main/java/org/the_chance/ui/product/ProductViewModel.kt
+++ user/ui/src/main/java/org/the_chance/ui/product/ProductViewModel.kt
@@ -4,11 +4,18 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import org.the_chance.ui.BaseViewModel
 
 @HiltViewModel
-class ProductViewModel : BaseViewModel(), CategoryProductInteractionListener {
+class ProductViewModel : BaseViewModel(), ProductInteractionListener ,
+    CategoryProductInteractionListener {
     override val TAG: String = this::class.java.simpleName
 
+
+    override fun onClickProduct(id: Int) {
+        TODO("Not yet implemented")
+    }
 
     override fun onClickCategoryProduct(id: Int) {
         TODO("Not yet implemented")
     }
+
+
 }
