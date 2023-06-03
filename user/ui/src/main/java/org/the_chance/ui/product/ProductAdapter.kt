868246baba82c1cfaaacd9b45efcaaca6bcacb3e
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-06-03 15:53:38 +0300

    create product adapter

--- user/ui/src/main/java/org/the_chance/ui/product/ProductAdapter.kt
+++ user/ui/src/main/java/org/the_chance/ui/product/ProductAdapter.kt
@@ -1,4 +1,13 @@
 package org.the_chance.ui.product
 
-class ProductAdapter {
+import org.the_chance.ui.BaseAdapter
+import org.the_chance.ui.BaseInteractionListener
+import org.the_chance.ui.R
+
+class ProductAdapter(listener: ProductInteractionListener) : BaseAdapter<ProductDto>(listener){
+    override val layoutID: Int = R.layout.item_category_product
+}
+
+interface ProductInteractionListener : BaseInteractionListener {
+    fun onClickProduct(id: Int)
 }
