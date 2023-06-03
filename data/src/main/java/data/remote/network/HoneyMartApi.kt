bfbea6d93cebb791ea02dd5e909a967a9d81b81a
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-06-03 03:11:26 +0300

    add fun get all market

--- data/src/main/java/data/remote/network/HoneyMartApi.kt
+++ data/src/main/java/data/remote/network/HoneyMartApi.kt
@@ -4,6 +4,7 @@ import data.remote.HoneyMartService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object HoneyMartApi {
 
--- data/src/main/java/data/remote/repository/HoneyMartRepositoryImp.kt
+++ data/src/main/java/data/remote/repository/HoneyMartRepositoryImp.kt
@@ -1,5 +1,15 @@
 package data.remote.repository
 
-class HoneyMartRepositoryImp {
+import data.remote.models.MarketDto
+import data.remote.network.HoneyMartApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class HoneyMartRepositoryImp : HoneyMartRepository {
+    private val honeyMartService = HoneyMartApi.honeyMartService
+    override fun getAllMart(): Flow<List<MarketDto>> = flow {
+        val response = honeyMartService.getMarkets()
+        emit(response.body()!!.value)
+    }
 
 }
