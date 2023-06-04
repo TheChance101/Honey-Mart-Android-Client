ca625be6d91b5fb9b96f9c39b6545a74de142c8c
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-04 16:19:25 +0300

    add all plugins

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,8 @@
+object Plugins {
+    const val ANDROID_APPLICATION = "com.android.application"
+    const val ANDROID_LIBRARY = "com.android.library"
+    const val HILT_LIBRARY = "com.google.dagger.hilt.android"
+    const val KOTLIN_ANDROID = "android"
+    const val KOTLIN = "kotlin"
+    const val KOTLIN_KAPT = "kapt"
+}
