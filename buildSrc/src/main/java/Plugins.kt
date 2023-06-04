39929592474a167d6581cfb496dc012a0401c059
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-04 17:37:10 +0300

    change plugin name

--- buildSrc/src/main/java/Plugins.kt
+++ buildSrc/src/main/java/Plugins.kt
@@ -3,6 +3,6 @@ object Plugins {
     const val ANDROID_LIBRARY = "com.android.library"
     const val HILT_LIBRARY = "com.google.dagger.hilt.android"
     const val KOTLIN_ANDROID = "android"
-    const val KOTLIN = "kotlin"
+    const val KOTLIN = "org.jetbrains.kotlin.jvm"
     const val KOTLIN_KAPT = "kapt"
 }
