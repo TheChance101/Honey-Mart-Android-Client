8c4940f8b7d862409831c2738a3bf2ab4c1fcdb7
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-06 12:46:42 +0300

    refactor Dependencies and change class name to DependencyVersions

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,56 +1,65 @@
 object Dependencies {
-    val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
-    val dataBindingDependency by lazy{"androidx.databinding:databinding-runtime:${Versions.dataBinding}"}
-    val junitDependency by lazy { "junit:junit:${Versions.JUNIT}" }
-    val androidTestDependencies by lazy {
-        listOf(
-            "androidx.test.espresso:espresso-core:${Versions.ESPRESSO}",
-            "androidx.test.ext:junit:${Versions.ANDROID_JUNIT}"
-        )
-    }
-    val navigationDependencies by lazy {
-        listOf(
-            "androidx.navigation:navigation-fragment-ktx:${Versions.ANDROID_NAVIGATION_FRAGMENT}",
-            "androidx.navigation:navigation-ui-ktx:${Versions.ANDROID_NAVIGATION_UI}"
-        )
-    }
+    // App Ui Dependencies
     val uiDependencies by lazy {
         listOf(
-            "androidx.core:core-ktx:${Versions.CORE_KTX}",
-            "androidx.appcompat:appcompat:${Versions.APP_COMPAT}",
-            "com.google.android.material:material:${Versions.MATERIAL}",
-            "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}"
+            "androidx.core:core-ktx:${DependencyVersions.CORE_KTX}",
+            "androidx.appcompat:appcompat:${DependencyVersions.APP_COMPAT}",
+            "com.google.android.material:material:${DependencyVersions.MATERIAL}",
+            "androidx.constraintlayout:constraintlayout:${DependencyVersions.CONSTRAINT_LAYOUT}"
+        )
+    }
+    val coreKtx by lazy { "androidx.core:core-ktx:${DependencyVersions.CORE_KTX}" }
+    val dataBindingDependency by lazy { "androidx.databinding:databinding-runtime:${DependencyVersions.DATA_BINDING}" }
+
+    // Test Dependencies
+    val junitDependency by lazy { "junit:junit:${DependencyVersions.JUNIT}" }
+    val androidTestDependencies by lazy {
+        listOf(
+            "androidx.test.espresso:espresso-core:${DependencyVersions.ESPRESSO}",
+            "androidx.test.ext:junit:${DependencyVersions.ANDROID_JUNIT}"
         )
     }
 
+    // navigation
+    val navigationDependencies by lazy {
+        listOf(
+            "androidx.navigation:navigation-fragment-ktx:${DependencyVersions.ANDROID_NAVIGATION_FRAGMENT}",
+            "androidx.navigation:navigation-ui-ktx:${DependencyVersions.ANDROID_NAVIGATION_UI}"
+        )
+    }
+
+    // Networking Dependencies
     val retrofitDependencies by lazy {
         listOf(
-            "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}",
-            "com.squareup.retrofit2:converter-gson:${Versions.RETROFIT}",
-            "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP_INTERCEPTOR}",
-            "com.google.code.gson:gson:${Versions.GSON}"
+            "com.squareup.retrofit2:retrofit:${DependencyVersions.RETROFIT}",
+            "com.squareup.retrofit2:converter-gson:${DependencyVersions.RETROFIT}",
+            "com.squareup.okhttp3:logging-interceptor:${DependencyVersions.OKHTTP_INTERCEPTOR}",
+            "com.google.code.gson:gson:${DependencyVersions.GSON}"
         )
-    }
-
-    val coroutinesDependency by lazy {
-        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.COROUTINES}"
     }
     val glideDependency by lazy {
-        "com.github.bumptech.glide:glide:${Versions.GLIDE}"
+        "com.github.bumptech.glide:glide:${DependencyVersions.GLIDE}"
     }
+
+    // Coroutines Dependencies
+    val coroutinesDependency by lazy {
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${DependencyVersions.COROUTINES}"
+    }
+
+    // Live Data
     val liveDataDependencies by lazy {
         listOf(
-            "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.LIVE_DATA_LIFE_CYCLE}",
-            "androidx.activity:activity-ktx:${Versions.LIVE_DATA_ACTIVITY}",
-            "androidx.fragment:fragment-ktx:${Versions.LIVE_DATA_FRAGMENT}"
+            "androidx.lifecycle:lifecycle-livedata-ktx:${DependencyVersions.LIVE_DATA_LIFE_CYCLE}",
+            "androidx.activity:activity-ktx:${DependencyVersions.LIVE_DATA_ACTIVITY}",
+            "androidx.fragment:fragment-ktx:${DependencyVersions.LIVE_DATA_FRAGMENT}"
         )
     }
+
+    // Hilt
     val hiltDependency by lazy {
-        "com.google.dagger:hilt-android:${Versions.HILT}"
+        "com.google.dagger:hilt-android:${DependencyVersions.HILT}"
     }
     val hiltCompiler by lazy {
-        "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+        "com.google.dagger:hilt-android-compiler:${DependencyVersions.HILT}"
     }
-
-
 }
--- buildSrc/src/main/java/DependencyVersions.kt
+++ buildSrc/src/main/java/DependencyVersions.kt
@@ -1,38 +1,19 @@
-object Versions {
-
-    const val appCompat = "1.6.1"
-    const val kotlin = "1.5.0"
-    const val coreKtx = "1.10.0"
-    const val material = "1.5.0"
-    const val constraintLayout = "2.1.4"
-    const val jUnit = "4.13.2"
-    const val androidJunit = "1.1.5"
-    const val espresso = "3.5.1"
-    const val okHttpInterceptor = "5.0.0-alpha.11"
-    const val retrofit = "2.9.0"
-    const val coroutines = "1.6.4"
-    const val gson = "2.10.1"
-    const val androidNavigationFragment = "2.5.3"
-    const val androidNavigationUi = "2.5.3"
-    const val glide = "4.15.1"
-    const val liveData = "2.6.1"
-    const val activity = "1.3.1"
-    const val dataBinding ="7.1.0"
-    const val fragment = "1.3.6"
-    const val hilt = "2.44"
-
-    // App Dependencies
+object DependencyVersions {
+    // App Ui Dependencies
     const val APP_COMPAT = "1.6.1"
     const val CORE_KTX = "1.10.0"
     const val MATERIAL = "1.5.0"
     const val CONSTRAINT_LAYOUT = "2.1.4"
+
     // Test Dependencies
     const val JUNIT = "4.13.2"
     const val ANDROID_JUNIT = "1.1.5"
     const val ESPRESSO = "3.5.1"
+
     // navigation
     const val ANDROID_NAVIGATION_FRAGMENT = "2.5.3"
     const val ANDROID_NAVIGATION_UI = "2.5.3"
+
     // Networking Dependencies
     const val OKHTTP_INTERCEPTOR = "5.0.0-alpha.11"
     const val RETROFIT = "2.9.0"
@@ -41,10 +22,11 @@ object Versions {
 
     // Coroutines Dependencies
     const val COROUTINES = "1.6.4"
+
     // Live Data
     const val LIVE_DATA_LIFE_CYCLE = "2.6.1"
     const val LIVE_DATA_ACTIVITY = "1.3.1"
     const val LIVE_DATA_FRAGMENT = "1.3.6"
     const val HILT = "2.44"
-
+    const val DATA_BINDING = "7.1.0"
 }
