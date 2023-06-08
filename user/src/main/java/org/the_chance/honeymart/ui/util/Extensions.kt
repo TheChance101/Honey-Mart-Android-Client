164b1362c2f0fd7001f2d57599f692c057f1765c
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-06-08 20:35:12 +0300

    enhance code

--- user/src/main/java/org/the_chance/honeymart/ui/util/Extensions.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/util/Extensions.kt
@@ -1,12 +1,12 @@
 package org.the_chance.honeymart.ui.util
 
 import android.content.Context
-import android.graphics.Color
 import android.view.View
 import android.view.Window
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.animation.ArgbEvaluatorCompat
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.imageview.ShapeableImageView
 import org.the_chance.design_system.R
@@ -16,26 +16,28 @@ fun RecyclerView.addOnScrollListenerWithAppbarColor(
     fragment: Fragment,
     appBarLayout: AppBarLayout,
 ) {
-    val threshold =
-        context.resources.getDimensionPixelSize(R.dimen.spacing_medium)
+    val threshold = context.resources.getDimensionPixelSize(R.dimen.spacing_medium)
     val window: Window = fragment.requireActivity().window
 
     addOnScrollListener(object : RecyclerView.OnScrollListener() {
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
             super.onScrolled(recyclerView, dx, dy)
+
             val offset = recyclerView.computeVerticalScrollOffset()
             val alpha = (offset.toFloat() / threshold).coerceIn(0f, 1f)
             val newColor = interpolateColor(
-                context,
-                R.color.white_300,
-                R.color.primary_100,
+                ContextCompat.getColor(context, R.color.white_300),
+                ContextCompat.getColor(context, R.color.primary_100),
                 alpha
             )
+
             appBarLayout.setBackgroundColor(newColor)
             window.statusBarColor = newColor
+
         }
     })
 }
+
 
 fun RecyclerView.addOnScrollListenerWithImageVisibility(
     imageDefault: ShapeableImageView,
@@ -52,9 +54,7 @@ fun RecyclerView.addOnScrollListenerWithImageVisibility(
                 }
 
                 RecyclerView.SCROLL_STATE_IDLE -> {
-                    // Scrolling has stopped
                     if (!recyclerView.canScrollVertically(-1)) {
-                        // Reached the top of the RecyclerView
                         imageDefault.visibility = View.VISIBLE
                         imageScrolled.visibility = View.GONE
                     }
@@ -64,11 +64,7 @@ fun RecyclerView.addOnScrollListenerWithImageVisibility(
     })
 }
 
-private fun interpolateColor(context: Context, colorRes1: Int, colorRes2: Int, ratio: Float): Int {
-    val color1 = ContextCompat.getColor(context, colorRes1)
-    val color2 = ContextCompat.getColor(context, colorRes2)
-    val r = (Color.red(color1) * (1 - ratio) + Color.red(color2) * ratio).toInt()
-    val g = (Color.green(color1) * (1 - ratio) + Color.green(color2) * ratio).toInt()
-    val b = (Color.blue(color1) * (1 - ratio) + Color.blue(color2) * ratio).toInt()
-    return Color.rgb(r, g, b)
+private fun interpolateColor(color1: Int, color2: Int, ratio: Float): Int {
+    val interpolatedColor = ArgbEvaluatorCompat.getInstance().evaluate(ratio, color1, color2)
+    return interpolatedColor.coerceAtLeast(color1).coerceAtMost(color2)
 }
