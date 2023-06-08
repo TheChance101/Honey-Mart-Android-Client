4233026445cab83fff6300d4572db8553a9e1ea7
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-08 20:28:42 +0300

    refactor extensions

--- user/src/main/java/org/the_chance/honeymart/ui/util/Extensions.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/util/Extensions.kt
@@ -9,15 +9,16 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.imageview.ShapeableImageView
+import org.the_chance.design_system.R
 
 fun RecyclerView.addOnScrollListenerWithAppbarColor(
-    context: Context, fragment: Fragment,
+    context: Context,
+    fragment: Fragment,
     appBarLayout: AppBarLayout,
 ) {
     val threshold =
-        context.resources.getDimensionPixelSize(org.the_chance.design_system.R.dimen.spacing_medium)
+        context.resources.getDimensionPixelSize(R.dimen.spacing_medium)
     val window: Window = fragment.requireActivity().window
-
 
     addOnScrollListener(object : RecyclerView.OnScrollListener() {
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
@@ -25,23 +26,15 @@ fun RecyclerView.addOnScrollListenerWithAppbarColor(
             val offset = recyclerView.computeVerticalScrollOffset()
             val alpha = (offset.toFloat() / threshold).coerceIn(0f, 1f)
             val newColor = interpolateColor(
-                ContextCompat.getColor(context, org.the_chance.design_system.R.color.white_300),
-                ContextCompat.getColor(context, org.the_chance.design_system.R.color.primary_100),
+                context,
+                R.color.white_300,
+                R.color.primary_100,
                 alpha
             )
             appBarLayout.setBackgroundColor(newColor)
-            val statusBarColor = interpolateColor(
-                ContextCompat.getColor(context, org.the_chance.design_system.R.color.white_300),
-                ContextCompat.getColor(context, org.the_chance.design_system.R.color.primary_100),
-                alpha
-            )
-            setStatusBarColor(window, statusBarColor)
+            window.statusBarColor = newColor
         }
     })
-}
-
-private fun setStatusBarColor(window: Window, color: Int) {
-    window.statusBarColor = color
 }
 
 fun RecyclerView.addOnScrollListenerWithImageVisibility(
@@ -52,26 +45,28 @@ fun RecyclerView.addOnScrollListenerWithImageVisibility(
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
 
+            when (newState) {
+                RecyclerView.SCROLL_STATE_DRAGGING -> {
+                    imageDefault.visibility = View.GONE
+                    imageScrolled.visibility = View.VISIBLE
+                }
 
-            if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
-                // Scrolling is in progress
-                imageDefault.visibility = View.GONE
-                imageScrolled.visibility = View.VISIBLE
-            } else if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                // Scrolling has stopped
-                if (!recyclerView.canScrollVertically(-1)) {
-                    // Reached the top of the RecyclerView
-                    imageDefault.visibility = View.VISIBLE
-                    imageScrolled.visibility = View.GONE
+                RecyclerView.SCROLL_STATE_IDLE -> {
+                    // Scrolling has stopped
+                    if (!recyclerView.canScrollVertically(-1)) {
+                        // Reached the top of the RecyclerView
+                        imageDefault.visibility = View.VISIBLE
+                        imageScrolled.visibility = View.GONE
+                    }
                 }
             }
-
-
         }
     })
 }
 
-private fun interpolateColor(color1: Int, color2: Int, ratio: Float): Int {
+private fun interpolateColor(context: Context, colorRes1: Int, colorRes2: Int, ratio: Float): Int {
+    val color1 = ContextCompat.getColor(context, colorRes1)
+    val color2 = ContextCompat.getColor(context, colorRes2)
     val r = (Color.red(color1) * (1 - ratio) + Color.red(color2) * ratio).toInt()
     val g = (Color.green(color1) * (1 - ratio) + Color.green(color2) * ratio).toInt()
     val b = (Color.blue(color1) * (1 - ratio) + Color.blue(color2) * ratio).toInt()
