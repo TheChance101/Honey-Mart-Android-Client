2a74d8e358d95f790b330ca2e53f4358cd25e1af
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-09 00:01:14 +0300

    move scroll listener to base fragment

--- user/src/main/java/org/the_chance/honeymart/ui/base/BaseFragment.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/base/BaseFragment.kt
@@ -10,6 +10,12 @@ import androidx.databinding.ViewDataBinding
 import androidx.databinding.library.baseAdapters.BR
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.imageview.ShapeableImageView
+import org.the_chance.honeymart.ui.util.addOnScrollListenerWithAppbarColor
+import org.the_chance.honeymart.ui.util.addOnScrollListenerWithImageVisibility
+import org.the_chance.user.R
 
 
 abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
@@ -18,6 +24,9 @@ abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
     abstract val layoutIdFragment: Int
     abstract val viewModel: ViewModel
     private lateinit var _binding: VB
+    private lateinit var appBarLayout: AppBarLayout
+    private lateinit var imageLogoDefault: ShapeableImageView
+    private lateinit var imageLogoScrolled: ShapeableImageView
     protected val binding get() = _binding
 
     override fun onCreateView(
@@ -34,7 +43,7 @@ abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
 
         _binding.apply {
             lifecycleOwner = viewLifecycleOwner
-            setVariable(BR.viewModel,viewModel)
+            setVariable(BR.viewModel, viewModel)
             return root
         }
     }
@@ -49,6 +58,21 @@ abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
     protected fun log(value: Any) {
         Log.e(TAG, value.toString())
     }
+
+    protected fun setupScrollListenerForRecyclerView(
+        recyclerView: RecyclerView,
+    ) {
+        appBarLayout = requireActivity().findViewById(R.id.appBarLayout)
+        imageLogoDefault = requireActivity().findViewById(R.id.image_logo)
+        imageLogoScrolled = requireActivity().findViewById(R.id.image_logo_scroll)
+        recyclerView.addOnScrollListenerWithAppbarColor(
+            requireContext(),
+            this,
+            appBarLayout
+        )
+        recyclerView.addOnScrollListenerWithImageVisibility(imageLogoDefault, imageLogoScrolled)
+    }
+
 }
 
 
--- user/src/main/java/org/the_chance/honeymart/ui/feature/market/MarketsFragment.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/market/MarketsFragment.kt
@@ -1,12 +1,8 @@
 package org.the_chance.honeymart.ui.feature.market
 
 import androidx.fragment.app.viewModels
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.imageview.ShapeableImageView
 import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.honeymart.ui.base.BaseFragment
-import org.the_chance.honeymart.ui.util.addOnScrollListenerWithAppbarColor
-import org.the_chance.honeymart.ui.util.addOnScrollListenerWithImageVisibility
 import org.the_chance.user.R
 import org.the_chance.user.databinding.FragmentMarketsBinding
 
@@ -15,22 +11,10 @@ class MarketsFragment : BaseFragment<FragmentMarketsBinding>() {
     override val TAG: String = "TAG"
     override val layoutIdFragment = R.layout.fragment_markets
     override val viewModel: MarketViewModel by viewModels()
-    private lateinit var appBarLayout: AppBarLayout
-    private lateinit var imageLogoDefault: ShapeableImageView
-    private lateinit var imageLogoScrolled: ShapeableImageView
-    override fun setup() {
 
+    override fun setup() {
         val adapter = MarketAdapter(viewModel)
         binding.recyclerMarkets.adapter = adapter
-
-        appBarLayout = requireActivity().findViewById(R.id.appBarLayout)
-        imageLogoDefault = requireActivity().findViewById(R.id.image_logo)
-        imageLogoScrolled = requireActivity().findViewById(R.id.image_logo_scroll)
-
-        binding.recyclerMarkets
-            .addOnScrollListenerWithAppbarColor(requireContext(),this,appBarLayout)
-        binding.recyclerMarkets
-            .addOnScrollListenerWithImageVisibility(imageLogoDefault, imageLogoScrolled)
-
+        setupScrollListenerForRecyclerView(binding.recyclerMarkets)
     }
 }
