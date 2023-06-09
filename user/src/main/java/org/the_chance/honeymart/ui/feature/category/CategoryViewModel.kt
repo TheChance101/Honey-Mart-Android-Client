952272c554b40c1b2541728aaefb99567a6fbccb
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-09 13:59:54 +0300

    refactor categories viewModel

--- user/src/main/java/org/the_chance/honeymart/ui/feature/category/CategoryViewModel.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/category/CategoryViewModel.kt
@@ -2,16 +2,16 @@ package org.the_chance.honeymart.ui.feature.category
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.update
-import org.the_chance.honeymart.domain.usecase.GetMarketAllCategoriesUseCase
+import org.the_chance.honeymart.domain.usecase.GetAllCategoriesInMarketUseCase
 import org.the_chance.honeymart.ui.base.BaseViewModel
 import org.the_chance.honeymart.ui.feature.uistate.CategoriesUiState
 import org.the_chance.honeymart.ui.feature.uistate.CategoryUiState
+import org.the_chance.honeymart.ui.feature.uistate.asCategoriesUiState
 import javax.inject.Inject
 
 @HiltViewModel
 class CategoryViewModel @Inject constructor(
-    private val getAllCategories: GetMarketAllCategoriesUseCase,
-  //  val id: Long,
+    private val getCategories: GetAllCategoriesInMarketUseCase,
 ) : BaseViewModel<CategoriesUiState>(CategoriesUiState()), CategoryInteractionListener {
     override val TAG: String = this::class.java.simpleName
 
@@ -20,27 +20,26 @@ class CategoryViewModel @Inject constructor(
         getAllCategory(1)
     }
 
-    private fun getAllCategory(id: Long) {
-        _uiState.update {it.copy(isLoading = true) }
+    private fun getAllCategory(marketId: Long) {
+        _uiState.update { it.copy(isLoading = true) }
         tryToExecute(
-            { getAllCategories(id)},
+            { getCategories(marketId) },
             { category -> category.asCategoriesUiState() },
             ::onSuccess,
             ::onError
         )
     }
 
-    private fun onSuccess(categories: List<CategoryUiState>){
+    private fun onSuccess(categories: List<CategoryUiState>) {
         this._uiState.update {
             it.copy(
                 isLoading = false,
-                isError = false,
                 categories = categories
             )
         }
     }
 
-    private fun onError(){
+    private fun onError() {
         this._uiState.update {
             it.copy(
                 isLoading = false,
