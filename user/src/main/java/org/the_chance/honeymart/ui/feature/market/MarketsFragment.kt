a72ca8a8cae8c9993c74c8783944e497d22457e4
Aziza <aziza.helmy35@gmail.com>
2023-06-09 22:20:29 +0300

    fix merging issues

--- user/src/main/java/org/the_chance/honeymart/ui/feature/market/MarketsFragment.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/market/MarketsFragment.kt
@@ -2,8 +2,6 @@ package org.the_chance.honeymart.ui.feature.market
 
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.imageview.ShapeableImageView
 import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.honeymart.ui.base.BaseFragment
 import org.the_chance.honeymart.ui.util.EventObserve
