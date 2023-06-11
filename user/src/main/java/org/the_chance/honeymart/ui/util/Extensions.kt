256abe2083abc0e2aeea483f599c41e36f911a3d
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-11 15:43:20 +0300

    remove color and themes, add return recycler extension handle toolbar and status bar color

--- user/src/main/java/org/the_chance/honeymart/ui/util/Extensions.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/util/Extensions.kt
@@ -1,9 +1,46 @@
 package org.the_chance.honeymart.ui.util
 
+import android.content.Context
 import android.view.View
-import android.widget.Toolbar
+import android.view.Window
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.animation.ArgbEvaluatorCompat
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.imageview.ShapeableImageView
+
+fun RecyclerView.addOnScrollListenerWithAppbarColor(
+    context: Context,
+    fragment: Fragment,
+    appBarLayout: AppBarLayout,
+) {
+    val threshold = context.resources.getDimensionPixelSize(org.the_chance.design_system.R.dimen.spacing_medium)
+    val window: Window = fragment.requireActivity().window
+
+    fun interpolateColor(color1: Int, color2: Int, ratio: Float): Int {
+        val interpolatedColor = ArgbEvaluatorCompat.getInstance().evaluate(ratio, color1, color2)
+        return interpolatedColor.coerceAtLeast(color1).coerceAtMost(color2)
+    }
+
+    addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val offset = recyclerView.computeVerticalScrollOffset()
+            val alpha = (offset.toFloat() / threshold).coerceIn(0f, 1f)
+            val newColor = interpolateColor(
+                ContextCompat.getColor(context, org.the_chance.design_system.R.color.white_300),
+                ContextCompat.getColor(context, org.the_chance.design_system.R.color.primary_100),
+                alpha
+            )
+
+            appBarLayout.setBackgroundColor(newColor)
+            window.statusBarColor = newColor
+
+        }
+    })
+}
 
 fun RecyclerView.addOnScrollListenerWithImageVisibility(
     imageDefault: ShapeableImageView,
