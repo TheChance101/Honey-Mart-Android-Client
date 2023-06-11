059f75a98f9c42cf9bb2966606840df3196a3688
Nour Elden Alaa <nourelden515@gmail.com>
2023-06-11 17:31:47 +0300

    change minApi level to 26

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object ConfigData{
     const val COMPILE_SDK_VERSION = 33
-    const val MIN_SDK_VERSION = 24
+    const val MIN_SDK_VERSION = 26
     const val TARGET_SDK_VERSION = 33
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
