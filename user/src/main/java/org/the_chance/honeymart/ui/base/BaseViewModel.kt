b4bd964698f02543ef4c0de1a80b738b06db877d
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-11 17:19:29 +0300

    add shared flow

--- user/src/main/java/org/the_chance/honeymart/ui/base/BaseViewModel.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/base/BaseViewModel.kt
@@ -5,11 +5,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import org.the_chance.honeymart.ui.util.EventHandler
 
-abstract class BaseViewModel<T>(initialState: T) : ViewModel() {
+abstract class BaseViewModel<T, E>(initialState: T) : ViewModel() {
 
 
     abstract val TAG: String
@@ -17,8 +20,11 @@ abstract class BaseViewModel<T>(initialState: T) : ViewModel() {
         Log.v(TAG, message)
     }
 
-    protected val _uiState = MutableStateFlow(initialState)
-    val uiState: StateFlow<T> = _uiState
+    protected val _state = MutableStateFlow(initialState)
+    val state = _state.asStateFlow()
+
+    protected val _effect = MutableSharedFlow<EventHandler<E>>()
+    val effect = _effect.asSharedFlow()
 
 
     protected fun <T, R> tryToExecute(
