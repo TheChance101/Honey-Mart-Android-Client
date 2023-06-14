9d9db5de18cb180241497e22146abec8156fbf6d
Ethaar hussien <ethaar7735@gmail.com>
2023-06-14 21:47:06 +0300

    add bottom nav

--- user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
@@ -2,7 +2,10 @@ package org.the_chance.honeymart.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.user.R
 import org.the_chance.user.databinding.ActivityMainBinding
@@ -17,5 +20,10 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_host) as NavHostFragment
         navHostFragment.navController
+
+        val navController = findNavController(R.id.fragment_host)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
