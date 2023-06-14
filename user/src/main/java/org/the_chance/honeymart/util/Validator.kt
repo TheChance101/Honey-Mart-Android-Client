edef59b3368887240e865d9cc9ca0b95ba743fb8
Narges-Nagy <nargesnagy21@gmail.com>
2023-06-15 01:29:33 +0300

    add validation for userName and password

--- user/src/main/java/org/the_chance/honeymart/util/Validator.kt
+++ user/src/main/java/org/the_chance/honeymart/util/Validator.kt
@@ -6,9 +6,18 @@ import java.util.regex.Pattern
  * Created by Aziza Helmy on 6/15/2023.
  */
 
-
 fun String.isEmail(): Boolean {
     return this.isNotEmpty() && Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
+        .matcher(this.fromArabic()).matches()
+}
+
+fun String.isPassword(): Boolean {
+    return (Pattern.compile("^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[@_^%\$#?*&!-]).{8,16}"))
+        .matcher(this.fromArabic()).matches()
+}
+
+fun String.isUserName(): Boolean {
+    return this.isNotEmpty() && this.length > 4 && (Pattern.compile("^[A-Za-z][A-Za-z0-9_]{4,14}\$")) //   ^[A-Za-z]\\w{4,14}$
         .matcher(this.fromArabic()).matches()
 }
 
