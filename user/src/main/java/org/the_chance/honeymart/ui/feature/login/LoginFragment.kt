3e11366d70b47a0f327cd6e7992f7e7401bfa7a9
Abdelrahman Mohamed <Abdelrahmanmohamed82@outlook.com>
2023-06-14 21:41:38 +0300

    login frag

---
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/login/LoginFragment.kt
@@ -0,0 +1,14 @@
+package org.the_chance.honeymart.ui.feature.login
+
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import org.the_chance.honeymart.ui.base.BaseFragment
+import org.the_chance.user.R
+import org.the_chance.user.databinding.FragmentLoginBinding
+
+@AndroidEntryPoint
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+    override val TAG: String = this::class.simpleName.toString()
+    override val layoutIdFragment = R.layout.fragment_signup
+    override val viewModel: LoginViewModel by viewModels()
+}
