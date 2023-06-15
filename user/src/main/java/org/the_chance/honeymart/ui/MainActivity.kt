ffe3cd892d6bef9646ecd9d2efe8881e2b1bcb60
Ethaar hussien <ethaar7735@gmail.com>
2023-06-15 10:14:30 +0300

    implementation code nav bottom bar

--- user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
@@ -2,28 +2,47 @@ package org.the_chance.honeymart.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.user.R
 import org.the_chance.user.databinding.ActivityMainBinding
 
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_host) as NavHostFragment
-        navHostFragment.navController
+        val navController = navHostFragment.navController
+        setupNavigation(navController)
+    }
 
+    override fun onResume() {
+        super.onResume()
+        findNavController(R.id.fragment_host)
+    }
+
+    private fun setupNavigation(navController: NavController) {
+        val navView = binding.bottomNavigationView
+        val topLevelDestinations = setOf(
+            R.id.marketsFragment,
+            R.id.categoriesFragment,
+            R.id.productsFragment
+        )
+
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragment_host)
-        NavigationUI.setupActionBarWithNavController(this, navController)
-
-        binding.bottomNavigationView.setupWithNavController(navController)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
