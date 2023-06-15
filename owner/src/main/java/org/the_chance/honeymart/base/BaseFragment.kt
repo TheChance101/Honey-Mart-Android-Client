5fb161a91c9fc49e55ae0d5f73ccb29f35f2894a
Aziza <aziza.helmy35@gmail.com>
2023-06-15 12:28:43 +0300

    add the needed bases in owner module

---
+++ owner/src/main/java/org/the_chance/honeymart/base/BaseFragment.kt
@@ -0,0 +1,53 @@
+package org.the_chance.honeymart.base
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+
+
+abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
+
+    abstract val TAG: String
+    abstract val layoutIdFragment: Int
+    abstract val viewModel: ViewModel
+    private lateinit var _binding: VB
+    protected val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            layoutIdFragment,
+            container, false
+        )
+
+        _binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+           // setVariable(BR.viewModel,viewModel)
+            return root
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
+
+    protected open fun setup() {}
+
+    protected fun log(value: Any) {
+        Log.e(TAG, value.toString())
+    }
+}
+
+
