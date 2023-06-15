dfe2d65f0adf8f0bf5bc006b18347670d3184de4
Aziza <aziza.helmy35@gmail.com>
2023-06-15 12:12:26 +0300

    add cart and orders fragments

---
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrdersFragment.kt
@@ -0,0 +1,20 @@
+package org.the_chance.honeymart.ui.feature.orders
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.the_chance.user.R
+
+class OrdersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_orders, container, false)
+    }
+
+}
