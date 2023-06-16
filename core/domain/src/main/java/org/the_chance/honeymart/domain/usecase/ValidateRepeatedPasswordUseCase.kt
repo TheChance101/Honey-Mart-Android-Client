d787268c10dec3833516c4a0e552b151be0d4597
Aziza <aziza.helmy35@gmail.com>
2023-06-16 21:20:21 +0300

    init commit of adding models

--- core/domain/src/main/java/org/the_chance/honeymart/domain/usecase/ValidateRepeatedPasswordUseCase.kt
+++ core/domain/src/main/java/org/the_chance/honeymart/domain/usecase/ValidateRepeatedPasswordUseCase.kt
@@ -1,6 +1,20 @@
 package org.the_chance.honeymart.domain.usecase
 
+import org.the_chance.honeymart.domain.util.ValidationResult
+
 /**
  * Created by Aziza Helmy on 6/16/2023.
  */
-class ValidateRepeatedPasswordUseCase
+class ValidateRepeatedPasswordUseCase{
+     fun execute(password: String, repeatedPassword: String): ValidationResult {
+        if(password != repeatedPassword) {
+            return ValidationResult(
+                isSuccessful  = false,
+                errorMsg  = "The passwords don't match"
+            )
+        }
+        return ValidationResult(
+            isSuccessful = true
+        )
+    }
+}
---
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/uistate/LoginUiState.kt
@@ -0,0 +1,11 @@
+package org.the_chance.honeymart.ui.feature.uistate
+
+/**
+ * Created by Aziza Helmy on 6/16/2023.
+ */
+
+data class LoginUiState(
+    val isLoading: Boolean = true,
+    val error: List<String> = emptyList()
+//TODO
+)
