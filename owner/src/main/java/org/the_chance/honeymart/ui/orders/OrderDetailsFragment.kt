8241a36594d2f15ba367285c5a75086574fec3d7
Haidy <haidyabugom3a@gmail.com>
2023-06-16 18:25:24 +0300

    add order details fragment

---
+++ owner/src/main/java/org/the_chance/honeymart/ui/orders/OrderDetailsFragment.kt
@@ -0,0 +1,17 @@
+package org.the_chance.honeymart.ui.orders
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.the_chance.owner.R
+
+class OrderDetailsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_orders, container, false)
+    }
+}
