9961ee09625dc5fd33c09aa1bddd6f66b7fe383b
Aziza <aziza.helmy35@gmail.com>
2023-06-16 22:47:18 +0300

    handle injection

--- core/domain/src/main/java/org/the_chance/honeymart/domain/usecase/LoginUserUseCase.kt
+++ core/domain/src/main/java/org/the_chance/honeymart/domain/usecase/LoginUserUseCase.kt
@@ -5,6 +5,9 @@ import javax.inject.Inject
 
 class LoginUserUseCase @Inject constructor(
     private val authRepository: AuthRepository,
+    private val validateEmailUseCase: ValidateEmailUseCase,
+    private val validatePasswordUseCase: ValidatePasswordUseCase
+
 ) {
     suspend operator fun invoke(email: String, password: String): String =
         authRepository.loginUser(email, password)
