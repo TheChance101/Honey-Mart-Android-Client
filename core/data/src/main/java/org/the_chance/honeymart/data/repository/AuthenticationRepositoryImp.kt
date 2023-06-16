a5ae44d907cff1c4cc0144405404390493b740ff
Aziza <aziza.helmy35@gmail.com>
2023-06-16 12:29:23 +0300

    add AuthenticationRepository

---
+++ core/data/src/main/java/org/the_chance/honeymart/data/repository/AuthenticationRepositoryImp.kt
@@ -0,0 +1,6 @@
+package org.the_chance.honeymart.data.repository
+
+/**
+ * Created by Aziza Helmy on 6/16/2023.
+ */
+class AuthenticationRepositoryImp
---
+++ core/domain/src/main/java/org/the_chance/honeymart/domain/repository/AuthenticationRepository.kt
@@ -0,0 +1,6 @@
+package org.the_chance.honeymart.domain.repository
+
+/**
+ * Created by Aziza Helmy on 6/16/2023.
+ */
+interface AuthenticationRepository
