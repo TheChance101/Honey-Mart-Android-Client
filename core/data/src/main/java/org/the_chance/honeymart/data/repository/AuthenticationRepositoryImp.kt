f2c6b600cdeeaea6afaca9050aa5099599bee98c
Aziza <aziza.helmy35@gmail.com>
2023-06-16 13:12:50 +0300

    handle and refactor

--- core/data/src/main/java/org/the_chance/honeymart/data/repository/AuthenticationRepositoryImp.kt
+++ core/data/src/main/java/org/the_chance/honeymart/data/repository/AuthenticationRepositoryImp.kt
@@ -3,4 +3,7 @@ package org.the_chance.honeymart.data.repository
 /**
  * Created by Aziza Helmy on 6/16/2023.
  */
-class AuthenticationRepositoryImp
+class AuthenticationRepositoryImp {
+    //TODO add user methods
+
+}
--- core/domain/src/main/java/org/the_chance/honeymart/domain/repository/AuthenticationRepository.kt
+++ core/domain/src/main/java/org/the_chance/honeymart/domain/repository/AuthenticationRepository.kt
@@ -3,4 +3,6 @@ package org.the_chance.honeymart.domain.repository
 /**
  * Created by Aziza Helmy on 6/16/2023.
  */
-interface AuthenticationRepository
+interface AuthenticationRepository{
+    //TODO add user methods
+}
