a25b806713159da4df25b867ede30902c63f4d25
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-06-17 03:50:12 +0300

    add change scrolling in market fragment

--- user/src/main/java/org/the_chance/honeymart/ui/feature/market/MarketsFragment.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/market/MarketsFragment.kt
@@ -1,10 +1,13 @@
 package org.the_chance.honeymart.ui.feature.market
 
-import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.imageview.ShapeableImageView
 import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.honeymart.ui.base.BaseFragment
+import org.the_chance.honeymart.util.addOnScrollListenerWithAppbarColor
+import org.the_chance.honeymart.util.addOnScrollListenerWithImageVisibility
 import org.the_chance.honeymart.util.collect
 import org.the_chance.user.R
 import org.the_chance.user.databinding.FragmentMarketsBinding
@@ -14,23 +17,26 @@ class MarketsFragment : BaseFragment<FragmentMarketsBinding>() {
     override val TAG: String = this::class.java.simpleName
     override val layoutIdFragment = R.layout.fragment_markets
     override val viewModel: MarketViewModel by viewModels()
+    private lateinit var appBarLayout: AppBarLayout
+    private lateinit var imageLogoDefault: ShapeableImageView
+    private lateinit var imageLogoScrolled: ShapeableImageView
     override fun setup() {
 
-        handleOnBackPressed()
-        initAdapter()
+        val adapter = MarketAdapter(viewModel)
+        binding.recyclerMarkets.adapter = adapter
+
+        appBarLayout = requireActivity().findViewById(R.id.appBarLayout)
+        imageLogoDefault = requireActivity().findViewById(R.id.image_logo)
+        imageLogoScrolled = requireActivity().findViewById(R.id.image_logo_scroll)
+
+        binding.recyclerMarkets
+            .addOnScrollListenerWithAppbarColor(requireContext(), this, appBarLayout)
+        binding.recyclerMarkets
+            .addOnScrollListenerWithImageVisibility(imageLogoDefault, imageLogoScrolled)
+
         collectAction()
     }
 
-    private fun initAdapter() {
-        val adapter = MarketAdapter(viewModel)
-        binding.recyclerMarkets.adapter = adapter
-    }
-
-    private fun handleOnBackPressed() {
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
-            requireActivity().finish()
-        }
-    }
 
     private fun collectAction() {
         collect(viewModel.effect) { effect ->
