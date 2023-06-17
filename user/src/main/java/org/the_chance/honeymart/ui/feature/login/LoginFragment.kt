35b44fab99ce01cb60cc9610f143d3d7315aff82
Nour Elden Alaa <nourelden515@gmail.com>
2023-06-17 16:02:35 +0300

    fix login navigation

--- user/src/main/java/org/the_chance/honeymart/ui/feature/login/LoginFragment.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/login/LoginFragment.kt
@@ -1,9 +1,12 @@
 package org.the_chance.honeymart.ui.feature.login
 
+import android.util.Log
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.honeymart.ui.base.BaseFragment
+import org.the_chance.honeymart.ui.feature.signup.SignupConfirmPasswordFragmentDirections
+import org.the_chance.honeymart.util.collect
 import org.the_chance.user.R
 import org.the_chance.user.databinding.FragmentLoginBinding
 
@@ -12,7 +15,21 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
     override val TAG: String = this::class.simpleName.toString()
     override val layoutIdFragment = R.layout.fragment_login
     override val viewModel: LoginViewModel by viewModels()
-    /*override fun setup() {
-        findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
-    }*/
+    override fun setup() {
+        collectAction()
+    }
+
+
+    private fun collectAction() {
+        collect(viewModel.effect) { effect ->
+            effect.getContentIfHandled()?.let { navigateToMainNav() }
+            Log.e("TAG", "collectAction: $effect")
+        }
+    }
+
+    private fun navigateToMainNav() {
+        val action =
+            LoginFragmentDirections.actionLoginFragmentPop()
+        findNavController().navigate(action)
+    }
 }
