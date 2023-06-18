30d114d3bbde35abdf95155b0b11e1d09b562aa8
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-18 14:09:46 +0300

    add position field

--- user/src/main/java/org/the_chance/honeymart/ui/feature/category/CategoryUiEffect.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/category/CategoryUiEffect.kt
@@ -1,6 +1,11 @@
 package org.the_chance.honeymart.ui.feature.category
 
-data class CategoryUiEffect(
+/*data class CategoryUiEffect(
     val categoryId: Long,
     val marketId: Long
+)*/
+data class CategoryUiEffect(
+    val categoryId: Long,
+    val marketId: Long,
+    val position: Int
 )
