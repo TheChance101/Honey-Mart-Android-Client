1d87614af4766b089d1ad62dd3e501c2b67f6f3c
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-19 23:53:34 +0300

    refactor dto

--- core/data/src/main/java/org/the_chance/honeymart/data/source/remote/models/OrderDto.kt
+++ core/data/src/main/java/org/the_chance/honeymart/data/source/remote/models/OrderDto.kt
@@ -1,17 +1,5 @@
 package org.the_chance.honeymart.data.source.remote.models
 
-import com.google.gson.annotations.SerializedName
-
-data class OrderDto1(
-    @SerializedName("orderId")
-    val orderId: Int,
-    @SerializedName("totalPrice")
-    val totalPrice: Double,
-    @SerializedName("state")
-    val state: Int,
-    @SerializedName("date")
-    val date: Long
-)
 data class OrderDto(
     val orderId: Int,
     val marketName: String,
