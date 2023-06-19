e8c42fd377403b2fc7a29a2a74fa22f0df7f831e
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-06-19 23:23:33 +0300

    add category id to ClickProductEffect

--- user/src/main/java/org/the_chance/honeymart/ui/feature/product/ProductUiEffect.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/product/ProductUiEffect.kt
@@ -1,6 +1,6 @@
 package org.the_chance.honeymart.ui.feature.product
 
 sealed class ProductUiEffect {
-    data class ClickProductEffect(val productId: Boolean) : ProductUiEffect()
+    data class ClickProductEffect(val productId: Long, val categoryId: Long) : ProductUiEffect()
     object UnAuthorizedUserEffect : ProductUiEffect()
 }
