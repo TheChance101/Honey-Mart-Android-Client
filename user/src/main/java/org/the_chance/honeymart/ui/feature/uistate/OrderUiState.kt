55e50ea3e045fab7faa49b8e24a75339e43b5c48
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-06-19 17:56:53 +0300

    create OrderUiState

--- user/src/main/java/org/the_chance/honeymart/ui/feature/uistate/OrderUiState.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/uistate/OrderUiState.kt
@@ -1,7 +1,25 @@
 package org.the_chance.honeymart.ui.feature.uistate
 
-data class OrderUiState(
+import org.the_chance.honeymart.domain.model.OrderEntity
+
+data class OrdersUiState(
     val isLoading: Boolean = true,
     val isError: Boolean = false,
+    val orders: List<OrderUiState> = emptyList()
 )
+
+data class OrderUiState(
+    val orderId: Int? = 0,
+    val marketName: String? = "",
+    val orderQuantity: String? = "",
+    val orderPrice: Double? = 0.0
+)
+
+fun OrderEntity.toOrderUiState(): OrderUiState {
+    return OrderUiState(
+        orderId = orderId,
+        marketName = marketName,
+        orderQuantity = orderQuantity,
+        orderPrice = orderPrice
+    )
 }
