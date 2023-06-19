27c9a4a234e06aa4942fbb023dce6b579f4f3c77
Ethaar hussien <ethaar7735@gmail.com>
2023-06-19 20:12:48 +0300

    add item orders

---
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrdersViewModel.kt
@@ -0,0 +1,7 @@
+package org.the_chance.honeymart.ui.feature.orders
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OrdersViewModel @Inject constructor()
