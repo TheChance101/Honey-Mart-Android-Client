ced3f07a289136b5d2358e7eba98bc278e6e3ae7
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-19 15:20:20 +0300

    implement order adapter

--- user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrdersAdapter.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrdersAdapter.kt
@@ -1,8 +1,12 @@
 package org.the_chance.honeymart.ui.feature.orders
 
 import org.the_chance.ui.BaseAdapter
+import org.the_chance.ui.BaseInteractionListener
 import org.the_chance.user.R
 
-class OrdersAdapter: BaseAdapter<> {
+class OrdersAdapter(listener: OrderInteractionListener) : BaseAdapter<OrderUiState>(listener) {
     override val layoutID: Int = R.layout.item_order
+}
+interface OrderInteractionListener : BaseInteractionListener {
+    fun onClickOrder(orderId: Long)
 }
