fcf27f79a508fe87bd64af329a7d25e112251bac
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-19 15:26:14 +0300

    add base viewModel

--- user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrderViewModel.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrderViewModel.kt
@@ -1,4 +1,19 @@
 package org.the_chance.honeymart.ui.feature.orders
 
-class OrderViewModel {
+import androidx.lifecycle.SavedStateHandle
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.the_chance.honeymart.ui.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OrderViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+) : BaseViewModel<OrdersUiState, Long>(OrdersUiState()), OrderInteractionListener, {
+    override val TAG: String = this::class.simpleName.toString()
+
+    override fun onClickOrder(orderId: Long) {
+        TODO("Not yet implemented")
+    }
+
 }
+
