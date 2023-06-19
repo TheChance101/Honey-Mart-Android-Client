fd3213120cf0e1e90c2867dfbc27bd853e92c03d
Abdelrahman Mohamed <Abdelrahmanmohamed82@outlook.com>
2023-06-19 16:11:44 +0300

    create use cases

---
+++ core/domain/src/main/java/org/the_chance/honeymart/domain/usecase/GetAllCartUseCase.kt
@@ -0,0 +1,12 @@
+package org.the_chance.honeymart.domain.usecase
+
+import org.the_chance.honeymart.domain.model.CartEntity
+import org.the_chance.honeymart.domain.repository.HoneyMartRepository
+import javax.inject.Inject
+
+class GetAllCartUseCase @Inject constructor(
+    private val honeyMartRepository: HoneyMartRepository
+) {
+    suspend operator fun invoke(): List<CartEntity> =
+        honeyMartRepository.getCart() ?: emptyList()
+}
