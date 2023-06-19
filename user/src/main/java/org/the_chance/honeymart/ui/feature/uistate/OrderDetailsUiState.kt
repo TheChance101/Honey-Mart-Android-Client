a5f6033dc4ea0f2bac5b654d7a66fbef995c83ad
Narges-Nagy <nargesnagy21@gmail.com>
2023-06-20 02:58:12 +0300

    add order details uistate and implement adapter

--- user/src/main/java/org/the_chance/honeymart/ui/feature/uistate/OrderDetailsUiState.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/uistate/OrderDetailsUiState.kt
@@ -1,2 +1,26 @@
 package org.the_chance.honeymart.ui.feature.uistate
 
+import org.the_chance.honeymart.domain.model.OrderDetailsEntity
+import java.util.*
+
+data class OrderDetailsUiState(
+    val isLoading: Boolean = true,
+    val isError: Boolean = false,
+    val products: List<OrderDetailsProductUiState> = emptyList(),
+)
+
+data class OrderDetailsProductUiState(
+    val products: List<Any>? = null,  // **********************
+    val totalPrice: Double? = 0.0,
+    val date: Date?= null,       // ****************
+    val state: Long? = 0
+)
+
+fun OrderDetailsEntity.OrderProductUiState(): OrderDetailsProductUiState {
+    return OrderDetailsProductUiState(
+        products = products,
+        totalPrice = totalPrice,
+        date = date,
+        state = state
+    )
+}
