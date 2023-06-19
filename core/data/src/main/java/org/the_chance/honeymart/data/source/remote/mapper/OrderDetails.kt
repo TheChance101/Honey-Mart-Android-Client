aed394c2d2c2b00889f7bc6cbbeab12a8e71222a
Narges-Nagy <nargesnagy21@gmail.com>
2023-06-20 01:49:17 +0300

    add order details mapper

--- core/data/src/main/java/org/the_chance/honeymart/data/source/remote/mapper/OrderDetails.kt
+++ core/data/src/main/java/org/the_chance/honeymart/data/source/remote/mapper/OrderDetails.kt
@@ -1,4 +1,11 @@
 package org.the_chance.honeymart.data.source.remote.mapper
 
-class OrderDetails {
-}
+import org.the_chance.honeymart.data.source.remote.models.OrderDetailsDto
+import org.the_chance.honeymart.domain.model.OrderDetailsEntity
+
+fun OrderDetailsDto.toOrderDetailsEntity() = OrderDetailsEntity(
+    products = products,
+    totalPrice = totalPrice,
+    date = date,
+    state = state
+)
