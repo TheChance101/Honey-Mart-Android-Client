a787193d5cbf9904f3c3bd55f409be3aa4b6f5e9
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-06-19 03:03:07 +0300

    create order adapter

--- user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrdersAdapter.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrdersAdapter.kt
@@ -1,4 +1,8 @@
 package org.the_chance.honeymart.ui.feature.orders
 
-class OrdersAdapter {
+import org.the_chance.ui.BaseAdapter
+import org.the_chance.user.R
+
+class OrdersAdapter: BaseAdapter<> {
+    override val layoutID: Int = R.layout.item_order
 }
