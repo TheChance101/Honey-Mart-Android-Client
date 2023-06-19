7c07bb84774d725377367f9331bec514db8c7ca7
Haidy <haidyabugom3a@gmail.com>
2023-06-19 18:43:11 +0300

    add product details design

---
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/product_details/ProductDetails.kt
@@ -0,0 +1,32 @@
+package org.the_chance.honeymart.ui.feature.product_details
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.the_chance.user.R
+
+class ProductDetails : Fragment() {
+
+    companion object {
+        fun newInstance() = ProductDetails()
+    }
+
+    private lateinit var viewModel: ProductDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_product_details, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProductDetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/product_details/ProductDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package org.the_chance.honeymart.ui.feature.product_details
+
+import androidx.lifecycle.ViewModel
+
+class ProductDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
