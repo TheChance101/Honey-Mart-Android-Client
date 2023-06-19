bf2a6f7bfcd17970a1d03a59a25743cc67d839a4
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-19 12:38:45 +0300

    update test token

--- core/data/src/main/java/org/the_chance/honeymart/data/source/remote/network/AuthInterceptor.kt
+++ core/data/src/main/java/org/the_chance/honeymart/data/source/remote/network/AuthInterceptor.kt
@@ -12,7 +12,7 @@ class AuthInterceptor @Inject constructor(
     private val dataStorePref: AuthDataStorePref
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val testToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiYXVkIjoiaW8ua3Rvci5zZXJ2ZXIuY29uZmlnLk1hcEFwcGxpY2F0aW9uQ29uZmlnQDIwMTViMmNkIiwiUk9MRV9UWVBFIjoiTm9ybWFsVXNlciIsImlzcyI6ImlvLmt0b3Iuc2VydmVyLmNvbmZpZy5NYXBBcHBsaWNhdGlvbkNvbmZpZ0AzNjU5ZDdiMSIsImV4cCI6MTY4NzE1NjMyN30.uv7iU8KCqiGXJg1hc5-1NmslKo62c6wVPZQL56gXT1c"
+        val testToken = "put your token here"
         val token = dataStorePref.getToken()
         val newRequest = chain
             .request()
