8311dd6dacaa1e9f3b7feda552518a6b697584e7
Ethaar hussien <ethaar7735@gmail.com>
2023-06-20 18:40:45 +0300

    add code fragment and view model order details

--- user/src/main/java/org/the_chance/honeymart/ui/feature/orderdetails/OrderDetailsFragment.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orderdetails/OrderDetailsFragment.kt
@@ -1,25 +1,35 @@
 package org.the_chance.honeymart.ui.feature.orderdetails
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import org.the_chance.honeymart.ui.base.BaseFragment
 import org.the_chance.user.R
+import org.the_chance.user.databinding.FragmentOrderDetailsBinding
 
-class OrderDetailsFragment : Fragment() {
+@AndroidEntryPoint
+class OrderDetailsFragment : BaseFragment<FragmentOrderDetailsBinding>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override val TAG: String = this::class.java.simpleName
+    override val layoutIdFragment = R.layout.fragment_order_details
+    override val viewModel: OrderDetailsViewModel by viewModels()
+    private val orderDetailsAdapter: OrderDetailsAdapter by lazy { OrderDetailsAdapter(viewModel) }
 
+    private fun initAdapters() {
+        binding.recyclerOrderDetails.adapter = orderDetailsAdapter
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_details, container, false)
-    }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_order_details, container, false)
+//    }
 
 }
--- user/src/main/java/org/the_chance/honeymart/ui/feature/orderdetails/OrderDetailsViewModel.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orderdetails/OrderDetailsViewModel.kt
@@ -1,4 +1,55 @@
 package org.the_chance.honeymart.ui.feature.orderdetails
 
-class OrderDetailsViewModel {
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import org.the_chance.honeymart.domain.usecase.GetOrderDetailsUseCase
+import org.the_chance.honeymart.domain.usecase.GetOrderProductsDetailsUseCase
+import org.the_chance.honeymart.ui.base.BaseViewModel
+import org.the_chance.honeymart.ui.feature.uistate.OrderDetailsProductUiState
+import org.the_chance.honeymart.ui.feature.uistate.OrderDetailsUiState
+import org.the_chance.honeymart.ui.feature.uistate.toOrderDetailsProductUiState
+import javax.inject.Inject
+
+@HiltViewModel
+class OrderDetailsViewModel @Inject constructor(
+    private val getOrderDetailsUseCase: GetOrderDetailsUseCase,
+    private val getOrderProductsDetailsUseCase: GetOrderProductsDetailsUseCase
+) : BaseViewModel<OrderDetailsUiState, Long>(OrderDetailsUiState()),
+    OrderDetailsInteractionListener {
+    override val TAG: String = this::class.java.simpleName
+
+    private fun getOrderProducts() {
+
+        _state.update { it.copy(isLoading = true) }
+        tryToExecute(
+            { getOrderProductsDetailsUseCase(1).map { it.toOrderDetailsProductUiState() } },
+            :: onGetOrderProductsSuccess,
+            :: onGetOrderProductsError
+        )
+    }
+
+    private fun onGetOrderProductsError(error: Exception) {
+        this._state.update {
+            it.copy(
+                isLoading = false,
+                isError = true,
+            )
+        }
+    }
+
+    private fun  onGetOrderProductsSuccess(products:List<OrderDetailsProductUiState>) {
+        _state.update {
+            it.copy(
+                isLoading = false,
+                isError = false,
+                products=products
+            )
+        }
+    }
+
+    override fun onClickProduct(productId: Long) {
+        TODO("Not yet implemented")
+    }
+
+
 }
