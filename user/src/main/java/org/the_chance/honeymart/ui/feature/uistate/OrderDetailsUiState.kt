8ce11cdc8a3036f63b6112ad5b820bc223133388
Ethaar hussien <ethaar7735@gmail.com>
2023-06-20 18:39:44 +0300

    edit order details ui state

--- user/src/main/java/org/the_chance/honeymart/ui/feature/uistate/OrderDetailsUiState.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/uistate/OrderDetailsUiState.kt
@@ -1,26 +1,41 @@
 package org.the_chance.honeymart.ui.feature.uistate
 
 import org.the_chance.honeymart.domain.model.OrderDetailsEntity
-import java.util.*
+import org.the_chance.honeymart.domain.model.OrderProductDetailsEntity
 
 data class OrderDetailsUiState(
-    val isLoading: Boolean = true,
+    val isLoading: Boolean = false,
     val isError: Boolean = false,
+    val orderDetails: OrderParentDetailsUiState = OrderParentDetailsUiState(),
     val products: List<OrderDetailsProductUiState> = emptyList(),
 )
 
-data class OrderDetailsProductUiState(
-    val products: List<Any>? = null,  // **********************
+data class OrderParentDetailsUiState(
     val totalPrice: Double? = 0.0,
-    val date: Date?= null,       // ****************
-    val state: Long? = 0
+    val date: String? = "",
+    val state: Int? = 0
 )
 
-fun OrderDetailsEntity.OrderProductUiState(): OrderDetailsProductUiState {
+data class OrderDetailsProductUiState(
+    val id: Long? = 0L,
+    val name: String? = "",
+    val count: Int? = 0,
+    val price: Double? = 0.0
+)
+
+fun OrderProductDetailsEntity.toOrderDetailsProductUiState(): OrderDetailsProductUiState {
     return OrderDetailsProductUiState(
-        products = products,
+        id = id,
+        name = name,
+        price = price,
+        count = count
+    )
+}
+
+fun OrderDetailsEntity.toOrderParentDetailsUiState(): OrderParentDetailsUiState {
+    return OrderParentDetailsUiState(
         totalPrice = totalPrice,
-        date = date,
-        state = state
+        state = state,
+        date = date
     )
 }
