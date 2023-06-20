ff6227d2f3e049eee09a4aff383a06422aca2152
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-06-20 13:27:23 +0300

    make state of ProductDetailsViewModel extend from ProductDetailsUiState

--- user/src/main/java/org/the_chance/honeymart/ui/feature/product_details/ProductDetailsViewModel.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/product_details/ProductDetailsViewModel.kt
@@ -5,8 +5,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.update
 import org.the_chance.honeymart.domain.usecase.GetProductByIdUseCase
 import org.the_chance.honeymart.ui.base.BaseViewModel
+import org.the_chance.honeymart.ui.feature.uistate.ProductDetailsUiState
 import org.the_chance.honeymart.ui.feature.uistate.ProductUiState
-import org.the_chance.honeymart.ui.feature.uistate.ProductsUiState
 import org.the_chance.honeymart.ui.feature.uistate.toProductUiState
 import javax.inject.Inject
 
@@ -14,36 +14,32 @@ import javax.inject.Inject
 class ProductDetailsViewModel @Inject constructor(
     private val getProductById: GetProductByIdUseCase,
     savedStateHandle: SavedStateHandle
-) : BaseViewModel<ProductsUiState, Long>(ProductsUiState()) {
+) : BaseViewModel<ProductDetailsUiState, Long>(ProductDetailsUiState()) {
 
     override val TAG: String = this::class.simpleName.toString()
     private val args = ProductDetailsArgs.fromSavedStateHandle(savedStateHandle)
 
     init {
         getProductByCategoryId(args.productId, args.categoryId)
-        log(args.productId.toString())
-        log(_state.value.products.toString())
     }
 
     private fun getProductByCategoryId(productId: Long, categoryId: Long) {
         _state.update { it.copy(isLoading = true) }
         tryToExecute(
-            { getProductById(productId, categoryId).map { it.toProductUiState() } },
+            { getProductById(productId, categoryId).toProductUiState() },
             ::onGetProductSuccess,
             ::onGetProductError,
         )
     }
 
-    private fun onGetProductSuccess(products: List<ProductUiState>) {
+    private fun onGetProductSuccess(product: ProductUiState) {
         _state.update {
             it.copy(
                 isLoading = false,
                 isError = false,
-                products = products
+                product = product
             )
         }
-        log(_state.value.products.toString())
-        log(products.toString())
     }
 
     private fun onGetProductError(throwable: Throwable) {
