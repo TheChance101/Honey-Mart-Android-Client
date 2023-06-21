4e0f674bf5f88691ef653c2a064b326f8b011cd1
Haidy <haidyabugom3a@gmail.com>
2023-06-21 19:28:40 +0300

    show snackBar

--- user/src/main/java/org/the_chance/honeymart/ui/feature/product_details/ProductDetailsFragment.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/product_details/ProductDetailsFragment.kt
@@ -1,11 +1,16 @@
 package org.the_chance.honeymart.ui.feature.product_details
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import org.the_chance.honeymart.ui.base.BaseFragment
+import org.the_chance.honeymart.util.collect
+import org.the_chance.honeymart.util.showSnackBar
 import org.the_chance.user.R
 import org.the_chance.user.databinding.FragmentProductDetailsBinding
+import java.lang.Exception
 
 @AndroidEntryPoint
 class ProductDetailsFragment : BaseFragment<FragmentProductDetailsBinding>() {
@@ -17,6 +22,7 @@ class ProductDetailsFragment : BaseFragment<FragmentProductDetailsBinding>() {
         setupUserFlowWindowVisibility()
         initiateAdapter()
         navigateBack()
+        collectEffect()
     }
 
     private fun initiateAdapter() {
@@ -29,4 +35,18 @@ class ProductDetailsFragment : BaseFragment<FragmentProductDetailsBinding>() {
             findNavController().navigateUp()
         }
     }
+
+    private fun collectEffect() {
+        collect(viewModel.effect) { effect ->
+            effect.getContentIfHandled()?.let { observeViewModelEvents(it) }
+        }
+    }
+
+    private fun observeViewModelEvents(effect: ProductDetailsEvent) {
+        when (effect) {
+            is ProductDetailsEvent.AddToCartSuccess -> { showSnackBar(effect.message) }
+            is ProductDetailsEvent.AddToCartError -> { showSnackBar(effect.error.toString()) }
+        }
+    }
+
 }
