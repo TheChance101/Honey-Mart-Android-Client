4cb3ff676783990bcebe9d1223a6a49ca2238791
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-22 20:57:33 +0300

    add images to wishList

--- core/data/src/main/java/org/the_chance/honeymart/data/source/remote/network/AuthInterceptor.kt
+++ core/data/src/main/java/org/the_chance/honeymart/data/source/remote/network/AuthInterceptor.kt
@@ -13,7 +13,7 @@ class AuthInterceptor @Inject constructor(
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val testToken =
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiYXVkIjoiaW8ua3Rvci5zZXJ2ZXIuY29uZmlnLk1hcEFwcGxpY2F0aW9uQ29uZmlnQDVlYzQ2Y2RkIiwiUk9MRV9UWVBFIjoiTm9ybWFsVXNlciIsImlzcyI6ImlvLmt0b3Iuc2VydmVyLmNvbmZpZy5NYXBBcHBsaWNhdGlvbkNvbmZpZ0AyMWQxYjMyMSIsImV4cCI6MTY4NzQ1MTIxNH0.UMMcC8BSsvCYdxiJFoKnNSyuZC_jibaEBnPyxhL5lHs"
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiYXVkIjoiaW8ua3Rvci5zZXJ2ZXIuY29uZmlnLk1hcEFwcGxpY2F0aW9uQ29uZmlnQGMxZmNhMmEiLCJST0xFX1RZUEUiOiJOb3JtYWxVc2VyIiwiaXNzIjoiaW8ua3Rvci5zZXJ2ZXIuY29uZmlnLk1hcEFwcGxpY2F0aW9uQ29uZmlnQDJjNzY4YWRhIiwiZXhwIjoxNjg3NDY2NTk1fQ.e4DIqZSfF6ppvQDiz7uzBjKuHaJNIo1xbblNSvo6xDg"
         val token = dataStorePref.getToken()
         val newRequest = chain
             .request()
