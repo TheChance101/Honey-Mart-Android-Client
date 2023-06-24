f942cda448837988d4cd820125efc0d842d8a93e
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-24 18:37:00 +0300

    remove used local url

--- user/src/main/java/org/the_chance/honeymart/di/NetworkModule.kt
+++ user/src/main/java/org/the_chance/honeymart/di/NetworkModule.kt
@@ -21,8 +21,8 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 internal object NetworkModule {
 
-    //private const val BASE_URL = "https://honey-mart-server-oe345.ondigitalocean.app/"
-    private const val BASE_URL = "http://192.168.1.10:8080/"
+    private const val BASE_URL = "https://honey-mart-server-oe345.ondigitalocean.app/"
+//    private const val BASE_URL = "http://192.168.1.10:8080/"
 
     @Singleton
     @Provides
