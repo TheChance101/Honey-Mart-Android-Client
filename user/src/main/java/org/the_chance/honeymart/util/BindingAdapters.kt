0bfcd4c6cefa1b31328bcf637c084a81ed8bba82
Abdelrahman Mohamed <Abdelrahmanmohamed82@outlook.com>
2023-06-27 15:54:17 +0300

    fix text of cat product

--- user/src/main/java/org/the_chance/honeymart/util/BindingAdapters.kt
+++ user/src/main/java/org/the_chance/honeymart/util/BindingAdapters.kt
@@ -221,28 +221,55 @@ fun showState(textView: TextView, state: Int) {
 @BindingAdapter("app:changeIfSelected")
 fun changeIfSelected(view: View, isSelected: Boolean) {
     val context = view.context
+    val uiManager =
+        context.applicationContext.getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+    when (uiManager.nightMode) {
+        UiModeManager.MODE_NIGHT_NO -> {
+            when (view) {
+                is CardView -> {
+                    val colorRes = if (isSelected) R.color.primary_100 else R.color.white_100
+                    val color = ContextCompat.getColor(context, colorRes)
+                    view.setCardBackgroundColor(color)
+                }
 
-    when (view) {
-        is CardView -> {
-            val colorRes = if (isSelected) R.color.primary_100 else R.color.white_100
-            val color = ContextCompat.getColor(context, colorRes)
-            view.setCardBackgroundColor(color)
+                is ShapeableImageView -> {
+                    val drawableRes =
+                        if (isSelected) R.drawable.icon_category_white else R.drawable.icon_category
+                    val drawable = ContextCompat.getDrawable(context, drawableRes)
+                    view.setImageDrawable(drawable)
+                }
+
+                is MaterialTextView -> {
+                    val colorRes = if (isSelected) R.color.primary_100 else R.color.white_100
+                    val color = ContextCompat.getColor(context, colorRes)
+                    view.setTextColor(color)
+                }
+            }
         }
+        UiModeManager.MODE_NIGHT_YES ->{
+            when (view) {
+                is CardView -> {
+                    val colorRes = if (isSelected) R.color.primary_100 else R.color.dark_background_400
+                    val color = ContextCompat.getColor(context, colorRes)
+                    view.setCardBackgroundColor(color)
+                }
 
-        is ShapeableImageView -> {
-            val drawableRes =
-                if (isSelected) R.drawable.icon_category_white else R.drawable.icon_category
-            val drawable = ContextCompat.getDrawable(context, drawableRes)
-            view.setImageDrawable(drawable)
-        }
+                is ShapeableImageView -> {
+                    val drawableRes =
+                        if (isSelected) R.drawable.icon_category_white else R.drawable.icon_category
+                    val drawable = ContextCompat.getDrawable(context, drawableRes)
+                    view.setImageDrawable(drawable)
+                }
 
-        is MaterialTextView -> {
-            val colorRes = if (isSelected) R.color.primary_100 else R.color.black_60
-            val color = ContextCompat.getColor(context, colorRes)
-            view.setTextColor(color)
+                is MaterialTextView -> {
+                    val colorRes = if (isSelected) R.color.primary_100 else R.color.dark_text_87
+                    val color = ContextCompat.getColor(context, colorRes)
+                    view.setTextColor(color)
+                }
+            }
         }
     }
-}
+    }
 
 @BindingAdapter("app:changeColorIfSelected")
 fun changeColorIfSelected(view: View, isFavorite: Boolean) {
