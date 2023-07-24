4a4536d779cafb43ffaf994202d4ae64df0fa618
Mennaakram <mennaakram976@gmail.com>
2023-07-25 02:24:49 +0300

    refactor auth screen

---
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/authentication/AurhScreen.kt
@@ -0,0 +1,90 @@
+package org.the_chance.honeymart.ui.feature.authentication
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.the_chance.design_system.R
+import org.the_chance.honymart.ui.composables.CustomButton
+import org.the_chance.honymart.ui.theme.Typography
+import org.the_chance.honymart.ui.theme.black37
+import org.the_chance.honymart.ui.theme.black60
+import org.the_chance.honymart.ui.theme.primary100
+
+@Composable
+fun AuthScreen() {
+    AuthContent(onClickSignUp = {})
+}
+
+@Composable
+fun AuthContent(
+    onClickSignUp: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(R.drawable.background_frame_auth),
+            contentDescription = "",
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = stringResource(R.string.welcome_to_honey_mart),
+            modifier = Modifier.padding(top = 16.dp),
+            color = black60,
+            style = Typography.displayMedium
+        )
+        Text(
+            text = stringResource(R.string.get_ready_to_a_shopping_experience_like_no_other),
+            modifier = Modifier.padding(top = 16.dp, start = 32.dp, end = 32.dp),
+            color = black37,
+            style = Typography.bodySmall,
+            textAlign = TextAlign.Center
+        )
+        CustomButton(
+            labelIdStringRes = R.string.Sign_up,
+            modifier = Modifier.padding(horizontal = 32.dp, vertical = 40.dp),
+            onClick = onClickSignUp
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 32.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.already_have_account),
+                color = black37,
+                style = Typography.displaySmall,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = stringResource(R.string.log_in),
+                color = primary100,
+                style = Typography.displayLarge,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun AuthPrev() {
+    AuthContent(onClickSignUp = {})
+}
--- user/src/main/java/org/the_chance/honeymart/ui/feature/authentication/AuthFragment.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/authentication/AuthFragment.kt
@@ -1,59 +1,38 @@
 package org.the_chance.honeymart.ui.feature.authentication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-import org.the_chance.honeymart.ui.base.BaseFragment
-import org.the_chance.honeymart.util.AuthData
-import org.the_chance.honeymart.util.collect
-import org.the_chance.user.R
-import org.the_chance.user.databinding.FragmentAuthBinding
+import org.the_chance.honeymart.ui.feature.login.LoginViewModel
+import org.the_chance.honymart.ui.theme.HoneyMartTheme
 
 @AndroidEntryPoint
-class AuthFragment : BaseFragment<FragmentAuthBinding>() {
+class AuthFragment : Fragment()  {
 
-    override val TAG: String = this::class.java.simpleName
-    override val layoutIdFragment: Int = R.layout.fragment_auth
-    override val viewModel: AuthViewModel by viewModels()
+    val viewModel: LoginViewModel by viewModels()
+    private lateinit var composeView: ComposeView
 
-    override fun setup() {
-        addCallbacks()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        setWindowVisibility(
-            appBarVisibility = false,
-            bottomNavVisibility = false,
-            isTransparentStatusBar = false,
-            isInAuthScreens = true
-        )
-    }
-    private fun addCallbacks() {
-        collectAction()
-    }
-
-    private fun collectAction() {
-        collect(viewModel.effect) { effect ->
-            effect.getContentIfHandled()?.let { onEffect(it) }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return ComposeView(requireActivity()).also {
+            composeView = it
         }
     }
 
-    private fun onEffect(effect: AuthenticationUiEffect) {
-        when (effect) {
-            is AuthenticationUiEffect.ClickLoginEffect -> navigateToLogin(effect.authData)
-            is AuthenticationUiEffect.ClickSignUpEffect -> navigateToSignUp(effect.authData)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        composeView.setContent {
+            HoneyMartTheme {
+                AuthScreen()
+            }
         }
     }
-
-    private fun navigateToLogin(authData: AuthData) {
-        val action = AuthFragmentDirections.actionAuthFragmentToLoginFragment(authData)
-        findNavController().navigate(action)
-    }
-
-    private fun navigateToSignUp(authData: AuthData) {
-        val action = AuthFragmentDirections.actionAuthFragmentToSignupFragment(authData)
-        findNavController().navigate(action)
-    }
-
 }
