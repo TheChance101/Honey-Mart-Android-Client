9947eda719a57fc479057f35a04894b7f68a7694
jinan <jinanaqb@gmail.com>
2023-07-25 04:56:13 +0300

    add order details composable

---
+++ design_system/src/main/java/org/the_chance/honymart/ui/composables/OrderDetailsCard.kt
@@ -0,0 +1,138 @@
+package org.the_chance.honymart.ui.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
+import org.the_chance.design_system.R
+import org.the_chance.honymart.ui.theme.HoneyMartTheme
+import org.the_chance.honymart.ui.theme.black37
+import org.the_chance.honymart.ui.theme.dimens
+import org.the_chance.honymart.ui.theme.white
+
+@Composable
+fun OrderDetailsCard(
+    imageUrl: String,
+    orderName: String,
+    orderPrice: String,
+    orderCount: String,
+    onClickCard: () -> Unit = {}
+) {
+    HoneyMartTheme {
+        Box(
+            modifier = Modifier
+                .size(width = 160.dp, height = 192.dp)
+                .clip(MaterialTheme.shapes.medium)
+                .clickable { onClickCard() }
+        ) {
+
+            SubcomposeAsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .crossfade(true)
+                    .build(),
+                loading = {
+                    CircularProgressIndicator()
+                },
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentScale = ContentScale.Crop,
+            )
+//            ImageNetwork(
+//                imageUrl = imageUrl
+//            )
+
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                black37,
+                            ),
+                            startY = 0f,
+                            endY = 300f
+                        )
+                    )
+            )
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(
+                        start = MaterialTheme.dimens.space8,
+                        bottom = MaterialTheme.dimens.space8,
+                        end = MaterialTheme.dimens.space8
+                    )
+            ) {
+                Text(
+                    text = orderName,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "$orderPrice$",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Row {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon_cart_check),
+                        contentDescription = "cart check icon",
+                        tint = white
+                    )
+                    SpacerHorizontal4()
+                    Text(
+                        text = "x$orderCount",
+                        style = MaterialTheme.typography.displaySmall,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun OrderDetailsCardPreview() {
+    OrderDetailsCard(
+        imageUrl = "https://img.freepik.com/free-photo/mid-century-modern-living-room-interior-design-with-monstera-tree_53876-129804.jpg",
+        orderName = "To Kill a Mockingbird",
+        orderPrice = "30,000",
+        orderCount = "3"
+    )
+}
