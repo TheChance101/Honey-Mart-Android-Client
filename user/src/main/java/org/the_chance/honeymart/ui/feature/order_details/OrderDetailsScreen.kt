cc9581c9dca054dbcae35ddefbb7e15c6c19ede5
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-07-25 16:02:52 +0300

    add OrderDetailsViewModel

--- user/src/main/java/org/the_chance/honeymart/ui/feature/order_details/OrderDetailsScreen.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/order_details/OrderDetailsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.MaterialTheme
@@ -13,26 +14,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import org.the_chance.honeymart.ui.feature.uistate.OrderDetailsUiState
 import org.the_chance.honymart.ui.composables.OrderDetailsCard
 import org.the_chance.honymart.ui.theme.dimens
 
 @Composable
 fun OrderDetailsScreen(
-//    viewModel: OrderDetailsViewModel = hiltViewModel()
+    viewModel: OrderDetailsViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.collectAsState().value
 
     OrderDetailsContent(
         state = state,
-        viewModel = viewModel,
     )
 }
 
 @Composable
 private fun OrderDetailsContent(
-    state: OrderDetailsUiState?,
-    viewModel: OrderDetailsViewModel,
+    state: OrderDetailsUiState,
 ) {
     Column() {
         LazyVerticalGrid(
@@ -43,9 +43,9 @@ private fun OrderDetailsContent(
             verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16),
             state = rememberLazyGridState(),
             content = {
-                itemsIndexed(state!!.products) { index, item ->
+                items(state.products){item ->
                     OrderDetailsCard(
-                        imageUrl = "https://img.freepik.com/free-photo/mid-century-modern-living-room-interior-design-with-monstera-tree_53876-129804.jpg",
+                        imageUrl = state.products.,
                         orderName = ,
                         orderPrice = "$34",
                         orderCount = "1"
