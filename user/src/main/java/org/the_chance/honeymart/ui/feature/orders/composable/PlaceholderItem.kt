6a3c31ea92882fd98cd9ae7b1f28d49e3e210bb2
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-25 14:33:35 +0300

    refactor PlaceholderItem using typography

--- user/src/main/java/org/the_chance/honeymart/ui/feature/orders/composable/PlaceholderItem.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orders/composable/PlaceholderItem.kt
@@ -3,20 +3,20 @@ package org.the_chance.honeymart.ui.feature.orders.composable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import org.the_chance.design_system.R
 import org.the_chance.honymart.ui.composables.CustomButton
-import org.the_chance.honymart.ui.theme.black37
+import org.the_chance.honymart.ui.theme.Typography
 import org.the_chance.honymart.ui.theme.black60
 import org.the_chance.honymart.ui.theme.primary100
 
@@ -33,24 +33,23 @@ fun PlaceholderItem(
         verticalArrangement = Arrangement.spacedBy(48.dp, Alignment.Top),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Image(painter = image, contentDescription = "PlaceHolder Image")
+        Image(
+            painter = image,
+            contentDescription = stringResource(org.the_chance.user.R.string.content_desc_of_placeholder_image)
+        )
         Column(
             verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Top),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
                 text = title,
-                fontWeight = FontWeight.SemiBold,
-                fontSize = 18.sp,
+                style = Typography.bodyMedium.copy(color = black60),
                 textAlign = TextAlign.Center,
-                color = black60
             )
             Text(
                 text = subtitle,
-                fontWeight = FontWeight.SemiBold,
-                fontSize = 14.sp,
+                style = Typography.displayLarge.copy(color = black60),
                 textAlign = TextAlign.Center,
-                color = black37
             )
         }
         CustomButton(
@@ -66,9 +65,10 @@ fun PlaceholderItem(
 @Composable
 fun PreviewPlaceholderItem() {
     PlaceholderItem(
+        modifier = Modifier.padding(horizontal = 32.dp, vertical = 24.dp),
         image = painterResource(id = R.drawable.no_orders_placeholder),
-        title = "You Don't have any orders!!",
-        subtitle = "It's a catastrophe! Let's fix it by adding items that catch your eye!",
+        title = stringResource(R.string.placeholder_title),
+        subtitle = stringResource(R.string.placeholder_subtitle),
         onClickDiscoverMarkets = { }
     )
 }
