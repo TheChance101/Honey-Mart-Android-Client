eee5409ed135d96588b959619cd81c0f890d4af0
TarekIdrees <tarekdrias321@gmail.com>
2023-07-26 00:18:28 +0300

    handle state of loading

--- user/src/main/java/org/the_chance/honeymart/ui/feature/product/ProductsScreen.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/product/ProductsScreen.kt
@@ -1,16 +1,16 @@
 package org.the_chance.honeymart.ui.feature.product
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,53 +28,8 @@ fun ProductsScreen(
     viewModel: ProductViewModel = hiltViewModel()
 ) {
     val state by viewModel.state.collectAsState()
-    ProductsContent(state = state)
-}
-@Composable
-private fun ProductsContent(state: ProductsUiState) {
-    if (state.isLoadingCategory || state.isLoadingProduct) {
 
-    } else if (state.isError) {
 
-    } else {
-        Column(modifier = Modifier.fillMaxSize()) {
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(start = 16.dp, end = 16.dp),
-                horizontalArrangement = Arrangement.spacedBy(12.dp)
-            ) {
-                LazyColumn(
-                    contentPadding = PaddingValues(top = 24.dp, end = 12.dp),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    items(state.categories.size) { index ->
-                        val category = state.categories[index]
-                        category.categoryImageId?.let { SideBarItem(icon = it) } // Use the appropriate icon property from CategoryUiState
-                    }
-                }
-                LazyColumn(
-                    contentPadding = PaddingValues(top = 24.dp),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    items(state.products.size) { index ->
-                        val product = state.products[index]
-                        ProductCard(
-                            imageUrl = product.productImages?.firstOrNull() ?: "",
-                            furnitureName = product.productName ?: "",
-                            furniturePrice = product.productPrice?.toString() ?: "",
-                            secondaryText = product.productDescription ?: ""
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewProducts() {
     val sampleState = ProductsUiState(
         isLoadingCategory = false,
         isLoadingProduct = false,
@@ -110,18 +65,112 @@ fun PreviewProducts() {
             )
         ),
         categories = listOf(
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
-            CategoryUiState(categoryImageId = R.drawable.ic_bed),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = true
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Kitchen \n Tools",
+                isCategorySelected = false
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = false
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = false
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = false
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = false
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = false
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = false
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = false
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = false
+            ),
+            CategoryUiState(
+                categoryImageId = R.drawable.ic_bed,
+                categoryName = "Bedroom",
+                isCategorySelected = false
+            ),
         )
     )
+
     ProductsContent(state = sampleState)
+}
+
+@Composable
+private fun ProductsContent(state: ProductsUiState) {
+    if (!state.isLoadingCategory && !state.isLoadingProduct) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 16.dp, end = 16.dp),
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            LazyColumn(
+                contentPadding = PaddingValues(top = 24.dp, end = 12.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(state.categories.size) { index ->
+                    val category = state.categories[index]
+                    SideBarItem(
+                        icon = category.categoryImageId!!,
+                        categoryName = category.categoryName!!,
+                        isSelected = category.isCategorySelected
+                    )  // Use the appropriate icon property from CategoryUiState
+                }
+            }
+            LazyColumn(
+                contentPadding = PaddingValues(top = 24.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(state.products.size) { index ->
+                    val product = state.products[index]
+                    ProductCard(
+                        imageUrl = product.productImages?.firstOrNull() ?: "",
+                        furnitureName = product.productName ?: "",
+                        furniturePrice = product.productPrice?.toString() ?: "",
+                        secondaryText = product.productDescription ?: ""
+                    )
+                }
+            }
+        }
+    }
+    LaunchedEffect(key1 = state.isLoadingProduct || state.isLoadingCategory) {
+        Log.d("Tarek", "${state.isLoadingProduct} ${state.isLoadingCategory}")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewProducts() {
+    //  ProductsContent(state = sampleState)
 }
