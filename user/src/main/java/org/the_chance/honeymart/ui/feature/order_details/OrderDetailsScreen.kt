9b9f54b267aa6471cdca5bc3bc33548517c21193
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-07-25 15:09:22 +0300

    create orderDetailsCompose

--- user/src/main/java/org/the_chance/honeymart/ui/feature/order_details/OrderDetailsScreen.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/order_details/OrderDetailsScreen.kt
@@ -8,45 +8,45 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.the_chance.honeymart.ui.feature.uistate.OrderDetailsUiState
 import org.the_chance.honymart.ui.composables.OrderDetailsCard
+import org.the_chance.honymart.ui.theme.dimens
 
 @Composable
 fun OrderDetailsScreen(
-//    viewModel: OrderDetailsViewModel = hiltViewModel(),
+//    viewModel: OrderDetailsViewModel = hiltViewModel()
 ) {
-//    val state = viewModel.state.collectAsState().value
+    val state = viewModel.state.collectAsState().value
 
     OrderDetailsContent(
-//        state = state,
-        state = null,
-//        viewModel = viewModel,
+        state = state,
+        viewModel = viewModel,
     )
-
 }
 
 @Composable
 private fun OrderDetailsContent(
     state: OrderDetailsUiState?,
-//    viewModel: OrderDetailsViewModel,
+    viewModel: OrderDetailsViewModel,
 ) {
     Column() {
         LazyVerticalGrid(
             columns = GridCells.Adaptive(minSize = 160.dp),
             modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(MaterialTheme.dimens.space16),
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16),
             state = rememberLazyGridState(),
             content = {
                 itemsIndexed(state!!.products) { index, item ->
                     OrderDetailsCard(
                         imageUrl = "https://img.freepik.com/free-photo/mid-century-modern-living-room-interior-design-with-monstera-tree_53876-129804.jpg",
-                        orderName = "",
+                        orderName = ,
                         orderPrice = "$34",
                         orderCount = "1"
                     )
@@ -55,7 +55,4 @@ private fun OrderDetailsContent(
             }
         )
     }
-
 }
-
-
