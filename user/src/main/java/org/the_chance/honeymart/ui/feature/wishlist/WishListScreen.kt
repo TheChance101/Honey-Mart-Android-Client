18d09111b8a43ed15efa32e92a6b6c33b204bc5e
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-07-25 20:44:23 +0300

    handle wish list screen

--- user/src/main/java/org/the_chance/honeymart/ui/feature/wishlist/WishListScreen.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/wishlist/WishListScreen.kt
@@ -1,13 +1,15 @@
 package org.the_chance.honeymart.ui.feature.wishlist
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -18,7 +20,7 @@ import org.the_chance.honymart.ui.composables.ItemFavorite
 fun WishListScreen(
     viewModel: WishListViewModel = hiltViewModel(),
 ) {
-    val state by viewModel.state.collectAsState()
+    val state = viewModel.state.collectAsState().value
     WishListContent(state = state)
 }
 
@@ -27,24 +29,28 @@ private fun WishListContent(
     state: WishListUiState,
 
     ) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        content = {
-            items(state.products) { productState ->
-                ItemFavorite(
-                    imageUrlMarket = productState.productImages!![0],
-                    name = productState.productName!!,
-                    price = "$${productState.productPrice}",
-                    description = "1",
-                    onClick = { }
-                )
-            }
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 160.dp),
+            contentPadding = PaddingValues(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            content = {
+                items(state.products) { productState ->
+                    ItemFavorite(
+                        imageUrlMarket = productState.productImages!![0],
+                        name = productState.productName!!,
+                        price = "$${productState.productPrice}",
+                        description = "1",
+                        onClick = { }
+                    )
+                }
 
 
-        })
+            })
+
+    }
+
 }
 
 @Preview(showSystemUi = true)
