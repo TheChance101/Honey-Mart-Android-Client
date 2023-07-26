4b61380619c2c90093940ff4463cb3455979cf8a
ameeramjed <pr.ameer98@gmail.com>
2023-07-26 15:51:31 +0300

    improve ProductDetailsScreen design implement

--- user/src/main/java/org/the_chance/honeymart/ui/feature/product_details/ProductDetailsScreen.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/product_details/ProductDetailsScreen.kt
@@ -3,6 +3,7 @@ package org.the_chance.honeymart.ui.feature.product_details
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -23,6 +25,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -120,8 +123,7 @@ private fun ProductDetailsContent(
 
                     Row(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .background(Color.Cyan),
+                            .fillMaxWidth(),
                         horizontalArrangement = Arrangement.SpaceBetween,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
@@ -134,7 +136,12 @@ private fun ProductDetailsContent(
 
                         Row {
                             CustomSmallIconButton(
-                                idIconDrawableRes = R.drawable.icon_remove_from_cart
+                                idIconDrawableRes = R.drawable.icon_remove_from_cart,
+                                contentColor = MaterialTheme.colorScheme.primary,
+                                background =  MaterialTheme.colorScheme.background,
+                                modifier = Modifier
+                                    .clip(CircleShape)
+                                    .border(1.dp, MaterialTheme.colorScheme.primary, CircleShape)
                             ) {}
 
                             Text(
