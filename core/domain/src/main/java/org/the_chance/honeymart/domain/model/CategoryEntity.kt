716c571a9f67d9d86263d1c7fe119660582861f9
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-07-26 20:06:17 +0300

    remove nullable from entity

--- core/domain/src/main/java/org/the_chance/honeymart/domain/model/CategoryEntity.kt
+++ core/domain/src/main/java/org/the_chance/honeymart/domain/model/CategoryEntity.kt
@@ -2,8 +2,8 @@ package org.the_chance.honeymart.domain.model
 
 
 data class CategoryEntity(
-    val categoryId: Long?,
-    val categoryName: String?,
-    val categoryImageId: Int?
+    val categoryId: Long,
+    val categoryName: String,
+    val categoryImageId: Int
 )
 
