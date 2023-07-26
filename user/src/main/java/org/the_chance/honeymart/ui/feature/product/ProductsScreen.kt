5c1175002f3862a0da8d020035ad978953d6dc76
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-07-26 20:09:31 +0300

    update products screen ui

--- user/src/main/java/org/the_chance/honeymart/ui/feature/product/ProductsScreen.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/product/ProductsScreen.kt
@@ -1,5 +1,6 @@
 package org.the_chance.honeymart.ui.feature.product
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -16,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import org.the_chance.design_system.R
 import org.the_chance.honeymart.ui.feature.uistate.ProductsUiState
+import org.the_chance.honymart.ui.composables.LottieLoadingAnimation
 import org.the_chance.honymart.ui.composables.ProductCard
 import org.the_chance.honymart.ui.composables.SideBarItem
 
@@ -28,6 +31,7 @@ fun ProductsScreen(
     ProductsContent(state = state, viewModel, viewModel)
 }
 
+@SuppressLint("SuspiciousIndentation")
 @Composable
 private fun ProductsContent(
     state: ProductsUiState,
@@ -35,9 +39,9 @@ private fun ProductsContent(
     categoryProductInteractionListener: CategoryProductInteractionListener
 ) {
     if (state.isLoadingCategory || state.isLoadingProduct) {
-
+        LottieLoadingAnimation()
     } else if (state.isError) {
-
+        Text(text = "Error")
     } else {
         Column(modifier = Modifier.fillMaxSize()) {
             Row(
@@ -53,21 +57,14 @@ private fun ProductsContent(
                 ) {
                     items(state.categories.size) { index ->
                         val category = state.categories[index]
-                        category.categoryImageId?.let {
                             SideBarItem(
                                 icon = R.drawable.ic_bed,
-                                categoryName = category.categoryName ?: "",
+                                categoryName = category.categoryName,
                                 isSelected = category.isCategorySelected,
                                 onClick = {
-                                    category.categoryId?.let { it1 ->
-                                        categoryProductInteractionListener.onClickCategory(
-                                            it1
-                                        )
-                                    }
+                                    categoryProductInteractionListener.onClickCategory(category.categoryId)
                                 }
-
                             )
-                        }
                     }
                 }
                 LazyColumn(
@@ -78,24 +75,16 @@ private fun ProductsContent(
                         val product = state.products[index]
                         ProductCard(
                             modifier = Modifier.fillMaxWidth(),
-                            imageUrl = product.productImages?.firstOrNull() ?: "",
-                            productName = product.productName ?: "",
-                            productPrice = product.productPrice?.toString() ?: "",
-                            secondaryText = product.productDescription ?: "",
-                            isFavoriteIconClicked = product.isFavorite!!,
+                            imageUrl = product.productImages.firstOrNull() ?: "",
+                            productName = product.productName,
+                            productPrice = product.productPrice.toString(),
+                            secondaryText = product.productDescription,
+                            isFavoriteIconClicked = product.isFavorite,
                             onClickCard = {
-                                product.productId?.let { it1 ->
-                                    productInteractionListener.onClickProduct(
-                                        it1
-                                    )
-                                }
+                                productInteractionListener.onClickProduct(product.productId)
                             },
                             onClickFavorite = {
-                                product.productId?.let { it1 ->
-                                    productInteractionListener.onClickFavIcon(
-                                        it1
-                                    )
-                                }
+                                productInteractionListener.onClickFavIcon(product.productId)
                             }
                         )
                     }
@@ -104,145 +93,5 @@ private fun ProductsContent(
         }
     }
 }
-//    val sampleState = ProductsUiState(
-//        isLoadingCategory = false,
-//        isLoadingProduct = false,
-//        isError = false,
-//        products = listOf(
-//            ProductUiState(
-//                productId = 1L,
-//                productName = "Sofa",
-//                productDescription = "Comfy sofa for your living room",
-//                productPrice = 30000.0,
-//                productImages = listOf("https://housing.com/news/wp-content/uploads/2022/11/living-room-furniture-design-compressed-1.jpg")
-//            ),
-//            ProductUiState(
-//                productId = 1L,
-//                productName = "Sofa",
-//                productDescription = "Comfy sofa for your living room",
-//                productPrice = 30000.0,
-//                productImages = listOf("https://housing.com/news/wp-content/uploads/2022/11/living-room-furniture-design-compressed-1.jpg")
-//            ),
-//            ProductUiState(
-//                productId = 1L,
-//                productName = "Sofa",
-//                productDescription = "Comfy sofa for your living room",
-//                productPrice = 30000.0,
-//                productImages = listOf("https://housing.com/news/wp-content/uploads/2022/11/living-room-furniture-design-compressed-1.jpg")
-//            ),
-//            ProductUiState(
-//                productId = 1L,
-//                productName = "Sofa",
-//                productDescription = "Comfy sofa for your living room",
-//                productPrice = 30000.0,
-//                productImages = listOf("https://housing.com/news/wp-content/uploads/2022/11/living-room-furniture-design-compressed-1.jpg")
-//            )
-//        ),
-//        categories = listOf(
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = true
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Kitchen \n Tools",
-//                isCategorySelected = false
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = false
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = false
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = false
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = false
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = false
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = false
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = false
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = false
-//            ),
-//            CategoryUiState(
-//                categoryImageId = R.drawable.ic_bed,
-//                categoryName = "Bedroom",
-//                isCategorySelected = false
-//            ),
-//        )
-//    )
-//
-//    ProductsContent(state = sampleState)
-//}
 
-//@Composable
-//private fun ProductsContent(state: ProductsUiState) {
-//    if (!state.isLoadingCategory && !state.isLoadingProduct) {
-//        Row(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(start = 16.dp, end = 16.dp),
-//            horizontalArrangement = Arrangement.Start,
-//        ) {
-//            LazyColumn(
-//                contentPadding = PaddingValues(top = 24.dp, end = 12.dp),
-//                verticalArrangement = Arrangement.spacedBy(16.dp)
-//            ) {
-//                items(state.categories.size) { index ->
-//                    val category = state.categories[index]
-//                    SideBarItem(
-//                        icon = category.categoryImageId!!,
-//                        categoryName = category.categoryName!!,
-//                        isSelected = category.isCategorySelected
-//                    )  // Use the appropriate icon property from CategoryUiState
-//                }
-//            }
-//            LazyColumn(
-//                contentPadding = PaddingValues(top = 24.dp),
-//                verticalArrangement = Arrangement.spacedBy(16.dp)
-//            ) {
-//                items(state.products.size) { index ->
-//                    val product = state.products[index]
-//                    ProductCard(
-//                        imageUrl = product.productImages?.firstOrNull() ?: "",
-//                        furnitureName = product.productName ?: "",
-//                        furniturePrice = product.productPrice?.toString() ?: "",
-//                        secondaryText = product.productDescription ?: ""
-//                    )
-//                }
-//            }
-//        }
-//    }
-//
-//}
 
-//@Preview
-//@Composable
-//fun PreviewProducts() {
-//    //  ProductsContent(state = sampleState)
-//}
