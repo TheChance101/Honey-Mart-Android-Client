c837d9766c086003232b32e95ddbacc8542942fc
TarekIdrees <tarekdrias321@gmail.com>
2023-07-27 21:46:02 +0300

    revert hexagonal shape

--- design_system/src/main/java/org/the_chance/honymart/ui/composables/SideBarItem.kt
+++ design_system/src/main/java/org/the_chance/honymart/ui/composables/SideBarItem.kt
@@ -1,20 +1,26 @@
 package org.the_chance.honymart.ui.composables
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import org.the_chance.design_system.R
+import androidx.compose.ui.unit.dp
 import org.the_chance.honymart.ui.theme.HoneyMartTheme
 import org.the_chance.honymart.ui.theme.dimens
-
 
 @Composable
 fun SideBarItem(
@@ -24,19 +30,29 @@ fun SideBarItem(
     isSelected: Boolean,
     onClick: () -> Unit = {}
 ) {
-
     HoneyMartTheme {
         Column(
-            modifier = modifier.width(MaterialTheme.dimens.space56),
+            modifier = Modifier.width(56.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space4)
         ) {
-            HexagonItem(
-                icon = icon,
-                hexagonSize = MaterialTheme.dimens.space56,
-                isSelected = isSelected,
+            IconButton(
+                modifier = modifier
+                    .size(MaterialTheme.dimens.space56)
+                    .background(
+                        if (isSelected) MaterialTheme.colorScheme.primary
+                        else  MaterialTheme.colorScheme.secondaryContainer,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .padding(bottom = MaterialTheme.dimens.space4),
                 onClick = onClick
-            )
+            ) {
+                Icon(
+                    painter = painterResource(id = icon),
+                    contentDescription = "icon",
+                    tint = if (isSelected) White else MaterialTheme.colorScheme.onSecondaryContainer
+                )
+            }
             Text(
                 text = categoryName,
                 style = MaterialTheme.typography.displaySmall,
@@ -54,5 +70,5 @@ fun SideBarItem(
 @Preview
 @Composable
 fun SideBarItemPreview() {
-    SideBarItem(categoryName = "Home", icon = R.drawable.ic_bed, isSelected = true)
+    // SideBarItem()
 }
