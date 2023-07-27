2a4c7239778af0340e8f2f2a6c9a699247176de0
Abdelrahman Mohamed <Abdelrahmanmohamed82@outlook.com>
2023-07-27 14:48:23 +0300

    Cart refactor

--- user/src/main/java/org/the_chance/honeymart/ui/feature/cart/CartScreen.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/cart/CartScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import org.the_chance.honeymart.ui.feature.cart.Composeables.CartCardView
@@ -33,12 +34,13 @@ import org.the_chance.honeymart.ui.feature.cart.Composeables.NoConnectionLayout
 import org.the_chance.honeymart.ui.feature.cart.screen.BottomSheetCompleteOrderContent
 import org.the_chance.honeymart.ui.feature.uistate.CartUiState
 import org.the_chance.honymart.ui.composables.CustomAlertDialog
+import org.the_chance.user.R
 
 @Composable
 fun CartScreen(
     viewModel: CartViewModel = hiltViewModel(),
     onClickButtonOrderDetails: () -> Unit = {},
-    onClickButtonDiscover : () -> Unit = {}
+    onClickButtonDiscover: () -> Unit = {}
 ) {
     val state by viewModel.state.collectAsState()
 
@@ -48,37 +50,29 @@ fun CartScreen(
         state.products.isEmpty() -> BottomSheetCompleteOrderContent(
             state = state,
             onClick = onClickButtonOrderDetails,
-            onClickButtonDiscover = onClickButtonDiscover
-        )
+            onClickButtonDiscover = onClickButtonDiscover)
         else -> CartContent(
             state = state,
-            onClickPlusOrder = {
-                viewModel.onClickAddCountProductInCart(it ?: 0)
-            },
-            onClickMinusOrder = {
-                viewModel.onClickMinusCountProductInCart(it ?: 0)
-            }
-            ,
+            onClickPlusOrder = { viewModel.onClickAddCountProductInCart(it ?: 0) },
+            onClickMinusOrder = { viewModel.onClickMinusCountProductInCart(it ?: 0) },
             deleteCart = viewModel::deleteCart
         )
     }
 }
-
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun CartContent(
     state: CartUiState,
     onClickPlusOrder: (Long?) -> Unit = {},
     onClickMinusOrder: (Long?) -> Unit = {},
-    deleteCart: Long.() -> Unit)
-{
+    deleteCart: Long.() -> Unit
+) {
     Box {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color.White)
         ) {
-
             LazyColumn(
                 modifier = Modifier.weight(1f),
                 contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
@@ -93,15 +87,18 @@ fun CartContent(
                         state = dismissState,
                         background = { SwipeBackGround() },
                         directions = setOf(DismissDirection.EndToStart),
-                        dismissContent = { CartItem(
-                            product = state.products[index],
-                            onClickMinus = onClickMinusOrder,
-                            onClickPlus = onClickPlusOrder
-                        ) })
+                        dismissContent = {
+                            CartItem(
+                                product = state.products[index],
+                                onClickMinus = onClickMinusOrder,
+                                onClickPlus = onClickPlusOrder
+                            )
+                        }
+                    )
+
                     if (showDialog) {
                         CustomAlertDialog(
-                            message = " you’re delete this product....\n" +
-                                    "Are you sure?",
+                            message = stringResource(R.string.you_re_delete_this_product_are_you_sure),
                             onConfirm = {
                                 deleteCart(index.toLong())
                                 showDialog = false
@@ -122,10 +119,7 @@ fun CartContent(
                     }
                 }
             }
-            CartCardView(
-                totalPrice = state.total.toString()
-            )
-
+            CartCardView(totalPrice = state.total.toString())
         }
     }
 }
