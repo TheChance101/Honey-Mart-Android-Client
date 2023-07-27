6f820f41ff0666929e4c720c8d588967fcc5368b
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-07-27 12:56:38 +0300

    add Loading Animation

--- user/src/main/java/org/the_chance/honeymart/ui/feature/wishlist/WishListScreen.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/wishlist/WishListScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -16,7 +17,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import org.the_chance.honeymart.ui.feature.uistate.WishListUiState
+import org.the_chance.honeymart.ui.feature.wishlist.compose.LoadingAnimation
+import org.the_chance.honeymart.ui.feature.wishlist.compose.NoConnectionError
+import org.the_chance.honeymart.ui.feature.wishlist.compose.PlaceHolderWishList
 import org.the_chance.honymart.ui.composables.ItemFavorite
+import org.the_chance.honymart.ui.theme.dimens
 
 @Composable
 fun WishListScreen(
@@ -24,42 +29,66 @@ fun WishListScreen(
 ) {
     val state = viewModel.state.collectAsState().value
     val lifecycleOwner = LocalLifecycleOwner.current
-    LaunchedEffect(lifecycleOwner) {
-        viewModel.getWishListProducts()
+
+    when {
+//        state.isLoading -> LoadingAnimation()
+        state.isError -> NoConnectionError{viewModel.getWishListProducts()}
+        state.products.isEmpty() -> PlaceHolderWishList { viewModel.onClickDiscoverButton() }
+        else -> {LaunchedEffect(lifecycleOwner) {
+            viewModel.getWishListProducts()
+        }
+
+        }
     }
-    WishListContent(state = state)
+    WishListContent(
+        state = state,
+        onClickFavoriteIcon = viewModel::onClickFavoriteIcon,
+        onClickProduct = viewModel::onClickProduct
+    )
 
 }
 
 @Composable
 private fun WishListContent(
     state: WishListUiState,
-
-    ) {
-
-
+    onClickFavoriteIcon: (Long) -> Unit,
+    onClickProduct: (Long) -> Unit,
+) {
     Column(modifier = Modifier.fillMaxSize()) {
+        if(state.isLoading){
+            LoadingAnimation()
+        }
         LazyVerticalGrid(
             columns = GridCells.Adaptive(minSize = 160.dp),
             modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(MaterialTheme.dimens.space16),
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16),
             content = {
                 items(state.products) { productState ->
                     ItemFavorite(
                         imageUrlMarket = productState.productImages!![0],
                         name = productState.productName!!,
-                        price = "$${productState.productPrice}",
-                        description = "1",
-                        onClick = { }
+                        price = "${productState.productPrice}",
+                        description = "${productState.description} ",
+                        onClickProduct = { onClickProduct(productState.productId!!) },
+                        onClickFavoriteIcon = { onClickFavoriteIcon(productState.productId!!) }
                     )
                 }
 
 
             })
-
     }
+//    if (state.isError) {
+//        NoConnectionError()
+//    } else if (state.isLoading) {
+//        Image(
+//            painter = rememberAsyncImagePainter(model = org.the_chance.design_system.R.raw.loading),
+//            contentDescription = ""
+//        )
+//    } else if (state.products.isEmpty()) {
+//        PlaceHolderWishList(vi)
+//    }
 
 }
 
