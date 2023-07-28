4659e37067ea599050b85df251ee9e06b2deadd1
jinan <jinanaqb@gmail.com>
2023-07-28 18:49:00 +0300

    show bottom nav bar

--- user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
@@ -1,14 +1,24 @@
 package org.the_chance.honeymart.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
+import org.the_chance.honeymart.ui.feature.bottom_navigation.BottomBar
+import org.the_chance.honymart.ui.theme.HoneyMartTheme
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
@@ -17,7 +27,19 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
         setContent {
-            UserNavigationApp()
+            CompositionLocalProvider(LocalNavigationProvider provides rememberNavController()) {
+                HoneyMartTheme() {
+                    val systemUiController = rememberSystemUiController()
+                    systemUiController.isSystemBarsVisible = false
+                    Scaffold(
+                        bottomBar = {
+                            BottomBar()
+                        }
+                    ) {
+                        UserNavigationApp()
+                    }
+                }
+            }
         }
     }
 }
