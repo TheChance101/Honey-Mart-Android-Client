87f295058f06fb700d44037f65a7c837b0cd4b80
jinan <jinanaqb@gmail.com>
2023-07-29 16:26:41 +0300

    delete cart id from order

--- user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrderRoute.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orders/OrderRoute.kt
@@ -1,37 +1,21 @@
 package org.the_chance.honeymart.ui.feature.orders
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import org.the_chance.honeymart.ui.navigation.Screen
 
 
 private val ROUTE = Screen.OrderScreen.route
 
-fun NavController.navigateToOrderScreen(cartId:Long) {
-    navigate("$ROUTE/$cartId")
+fun NavController.navigateToOrderScreen() {
+    navigate("$ROUTE")
 }
 
 fun NavGraphBuilder.orderRoute() {
     composable(
-        route = "${ROUTE}/${OrderArgs.CART_ID}",
-        arguments = listOf(
-            navArgument(name = OrderArgs.CART_ID) {
-                NavType.LongType
-            }
-        )
+        route = "$ROUTE",
     ) {
         OrdersScreen()
-    }
-}
-
-class OrderArgs(savedStateHandle: SavedStateHandle) {
-    val cartId: String = checkNotNull(savedStateHandle[CART_ID])
-
-    companion object {
-        const val CART_ID = "cartId"
     }
 }
