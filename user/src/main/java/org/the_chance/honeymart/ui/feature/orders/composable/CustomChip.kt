0a4d36ca46770868b22e1b24b26c09d7b8ac934a
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-30 17:54:16 +0300

    update chips change to card

--- user/src/main/java/org/the_chance/honeymart/ui/feature/orders/composable/CustomChip.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orders/composable/CustomChip.kt
@@ -2,20 +2,24 @@ package org.the_chance.honeymart.ui.feature.orders.composable
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.the_chance.honymart.ui.theme.Typography
 import org.the_chance.honymart.ui.theme.primary100
+import org.the_chance.honymart.ui.theme.white
 
 @Composable
 fun CustomChip(
@@ -24,19 +28,25 @@ fun CustomChip(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    OutlinedButton(
-        modifier = modifier.clickable { onClick() },
-        colors = if (state) ButtonDefaults.buttonColors(primary100)
-        else ButtonDefaults.buttonColors(Transparent),
+    val interactionSource = remember { MutableInteractionSource() }
+    Card(
+        modifier = modifier.clickable(
+            interactionSource = interactionSource,
+            indication = null
+        ) {
+            onClick()
+        },
+        colors = if (state) CardDefaults.cardColors(primary100)
+        else CardDefaults.cardColors(white),
         border = if (state) BorderStroke(width = 0.dp, color = Transparent)
         else BorderStroke(width = 1.dp, color = primary100),
-        onClick = onClick,
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 6.dp)
+        shape = CircleShape
     ) {
         Text(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 6.dp),
             text = text,
             color = if (state) Color.White else primary100,
-            style = Typography.displayLarge
+            style = MaterialTheme.typography.displayLarge
         )
     }
 }
