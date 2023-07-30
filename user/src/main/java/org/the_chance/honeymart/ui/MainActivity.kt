5cf33d08cc4afd4c346843b6c2dc508bbb8f1978
TarekIdrees <tarekdrias321@gmail.com>
2023-07-30 14:13:55 +0300

    resolve layout flaying

--- user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
@@ -2,15 +2,12 @@ package org.the_chance.honeymart.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.honeymart.ui.feature.bottom_navigation.BottomBar
 import org.the_chance.honeymart.ui.navigation.UserNavGraph
@@ -22,10 +19,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
         setContent {
             CompositionLocalProvider(LocalNavigationProvider provides rememberNavController()) {
                 HoneyMartTheme() {
