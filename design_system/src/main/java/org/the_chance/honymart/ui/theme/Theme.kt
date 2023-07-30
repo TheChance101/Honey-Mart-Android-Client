e558bb1afea4ac8e4f546f719342f38f56a70404
Nour Elden Alaa <nourelden515@gmail.com>
2023-07-30 17:41:28 +0300

    make status bar transparent

--- design_system/src/main/java/org/the_chance/honymart/ui/theme/Theme.kt
+++ design_system/src/main/java/org/the_chance/honymart/ui/theme/Theme.kt
@@ -8,12 +8,18 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val localDimens = compositionLocalOf { Dimens() }
 
@@ -47,15 +53,37 @@ val MaterialTheme.dimens: Dimens
 fun HoneyMartTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = true,
+    useDarkIcons :Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    val systemUiController = rememberSystemUiController()
+
+    DisposableEffect(systemUiController, useDarkIcons) {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = useDarkIcons,
+            isNavigationBarContrastEnforced = false
+        )
+
+        onDispose {}
+    }
 
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            val window = (view.context as Activity).window
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+
+            ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { view, windowInsets ->
+                val insets = windowInsets.getInsets(
+                    WindowInsetsCompat.Type.systemGestures()
+                )
+                view.updatePadding(
+                    bottom = insets.bottom
+                )
+                WindowInsetsCompat.CONSUMED
+            }
         }
     }
 
