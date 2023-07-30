c7e29d1c20063102627a8c4eb4eee8a41473b577
Nour Elden Alaa <nourelden515@gmail.com>
2023-07-30 23:37:35 +0300

    fix padding in login Screen

--- user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -36,9 +38,15 @@ class MainActivity : AppCompatActivity() {
                     Scaffold(
                         bottomBar = {
                             BottomBar(bottomNavState)
-                        }
+                        },
+                        contentWindowInsets = WindowInsets(0, 0, 0, 0)
                     ) { innerPadding ->
-                        Box(modifier = Modifier.padding(innerPadding)) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                        )
+                        {
                             MainNavGraph()
                         }
                     }
