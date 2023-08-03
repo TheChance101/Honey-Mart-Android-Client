2e8eb46db22114366a4ec2330b03f0e542515c78
Aziza <aziza.helmy35@gmail.com>
2023-08-04 01:57:32 +0300

    enhance HoneyOutlineButton.kt

--- design_system/src/main/java/org/the_chance/honymart/ui/composables/HoneyOutlineButton.kt
+++ design_system/src/main/java/org/the_chance/honymart/ui/composables/HoneyOutlineButton.kt
@@ -1,5 +1,6 @@
 package org.the_chance.honymart.ui.composables
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
@@ -10,9 +11,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.the_chance.design_system.R
 import org.the_chance.honymart.ui.theme.HoneyMartTheme
+import org.the_chance.honymart.ui.theme.Shapes
 import org.the_chance.honymart.ui.theme.Typography
 import org.the_chance.honymart.ui.theme.dimens
 
@@ -22,15 +26,20 @@ fun HoneyOutlineButton(
     label: String,
     modifier: Modifier = Modifier,
     isEnable: Boolean = true,
-    contentColor: Color = MaterialTheme.colorScheme.onPrimary,
-    background: Color = MaterialTheme.colorScheme.primary,
+    contentColor: Color = MaterialTheme.colorScheme.primary,
+    background: Color = Color.Transparent,
 ) {
     Button(
         onClick = onClick,
         enabled = isEnable,
         modifier = modifier
             .wrapContentWidth()
-            .height(MaterialTheme.dimens.heightPrimaryButton),
+            .height(MaterialTheme.dimens.heightPrimaryButton)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.primary,
+                shape = Shapes.medium
+            ),
         colors = ButtonDefaults.buttonColors(
             containerColor = background,
             contentColor = contentColor,
@@ -40,7 +49,7 @@ fun HoneyOutlineButton(
     ) {
         Text(
             text = label,
-            style = Typography.bodyMedium,
+            style = Typography.bodySmall.copy(fontWeight = FontWeight.Normal),
             color = contentColor
         )
     }
