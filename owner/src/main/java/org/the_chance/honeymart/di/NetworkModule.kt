55c0bb724617a35e012dc551445739b107938a1c
Aziza <aziza.helmy35@gmail.com>
2023-08-04 14:58:59 +0300

    add di module in owner app

---
+++ owner/src/main/java/org/the_chance/honeymart/di/NetworkModule.kt
@@ -0,0 +1,78 @@
+package org.the_chance.honeymart.di
+
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.gson.gson
+import okhttp3.logging.HttpLoggingInterceptor
+import org.the_chance.honeymart.data.source.local.AuthDataStorePref
+import org.the_chance.honeymart.data.source.remote.network.AuthInterceptor
+import org.the_chance.honeymart.data.source.remote.network.HoneyMartService
+import org.the_chance.honeymart.data.source.remote.network.HoneyMartServiceImp
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    private const val BASE_URL = "https://honey-mart-server-oe345.ondigitalocean.app/"
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor,
+    ): HttpClient {
+        return HttpClient(OkHttp) {
+            expectSuccess = true
+
+            engine {
+                addInterceptor(loggingInterceptor)
+                addInterceptor(authInterceptor)
+                config {
+                    retryOnConnectionFailure(true)
+                    connectTimeout(1, TimeUnit.MINUTES)
+                    readTimeout(1, TimeUnit.MINUTES)
+                    writeTimeout(1, TimeUnit.MINUTES)
+                }
+            }
+            defaultRequest {
+                url(BASE_URL)
+                header(HttpHeaders.ContentType, "application/json")
+            }
+
+            install(ContentNegotiation) {
+                gson()
+            }
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideHoneyMartService(httpClient: HttpClient): HoneyMartService {
+        return HoneyMartServiceImp(httpClient)
+    }
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BASIC
+    }
+
+    @Singleton
+    @Provides
+    fun provideHeaderInterceptor(dataStorePref: AuthDataStorePref): AuthInterceptor {
+        return AuthInterceptor(dataStorePref)
+    }
+
+}
---
+++ owner/src/main/java/org/the_chance/honeymart/di/RepositoryModule.kt
@@ -0,0 +1,34 @@
+package org.the_chance.honeymart.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.the_chance.honeymart.data.repository.AuthRepositoryImp
+import org.the_chance.honeymart.data.repository.HoneyMartRepositoryImp
+import org.the_chance.honeymart.data.source.local.AuthDataStorePref
+import org.the_chance.honeymart.data.source.remote.network.HoneyMartService
+import org.the_chance.honeymart.domain.repository.AuthRepository
+import org.the_chance.honeymart.domain.repository.HoneyMartRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object RepositoryModule {
+    @Singleton
+    @Provides
+    fun bindHoneyMartRepository(
+        honeyMartService: HoneyMartService,
+    ): HoneyMartRepository {
+        return HoneyMartRepositoryImp(honeyMartService)
+    }
+
+    @Singleton
+    @Provides
+    fun bindAuthRepository(
+        authDataStorePrefImp: AuthDataStorePref,
+        honeyMartService: HoneyMartService,
+    ): AuthRepository {
+        return AuthRepositoryImp(authDataStorePrefImp, honeyMartService)
+    }
+}
