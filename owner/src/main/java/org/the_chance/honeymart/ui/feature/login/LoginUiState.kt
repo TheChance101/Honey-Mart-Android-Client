6dd4e7e6c095ffb1b59f25944a33a4d01e35dc0e
jinan <jinanaqb@gmail.com>
2023-08-05 19:15:53 +0300

    owner | add login ui state

---
+++ owner/src/main/java/org/the_chance/honeymart/ui/feature/login/LoginUiState.kt
@@ -0,0 +1,14 @@
+package org.the_chance.honeymart.ui.feature.login
+
+import org.the_chance.honeymart.domain.util.ErrorHandler
+import org.the_chance.honeymart.domain.util.ValidationState
+
+data class LoginUiState(
+    val isLoading: Boolean = false,
+    val error: ErrorHandler? = null,
+    val email: String = "",
+    val password: String = "",
+    val emailState: ValidationState = ValidationState.VALID_EMAIL,
+    val passwordState: ValidationState = ValidationState.VALID_PASSWORD,
+    val validationState: ValidationState = ValidationState.BLANK_EMAIL,
+)
