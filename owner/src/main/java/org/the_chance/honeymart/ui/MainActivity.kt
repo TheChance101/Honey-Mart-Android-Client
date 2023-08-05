55ffa8215f349ee34115a58a98e58850459134cc
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-08-05 03:07:16 +0300

    Owner | add AndroidEntryPoint and setContent for compose

--- owner/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package org.the_chance.honeymart.ui
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.owner.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+
+        }
     }
 }
