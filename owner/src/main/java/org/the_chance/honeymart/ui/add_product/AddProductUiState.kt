a1190e62c35612896f2500b11380d047cd5298de
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-08-06 18:46:35 +0300

    Owner | add mapper from product entity to product ui state

--- owner/src/main/java/org/the_chance/honeymart/ui/add_product/AddProductUiState.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/add_product/AddProductUiState.kt
@@ -1,14 +1,23 @@
 package org.the_chance.honeymart.ui.add_product
 
-import android.net.Uri
+import org.the_chance.honeymart.domain.model.ProductEntity
 import org.the_chance.honeymart.domain.util.ErrorHandler
 
 data class AddProductUiState(
     val isLoading: Boolean = false,
+    val isError: Boolean = false,
     val error: ErrorHandler? = null,
     val productName: String = "",
-    val numberOfItems: Int = 0,
-    val price: Double = 0.0,
-    val description: String = "",
-    val productImages: List<Uri> = emptyList(),
+    val productPrice: String = "",
+    val productDescription: String = "",
+    val productImages: List<String> = emptyList(),
 )
+
+fun ProductEntity.toAddProductUiState(): AddProductUiState {
+    return AddProductUiState(
+        productName = productName,
+        productPrice = ProductPrice.toString(),
+        productDescription = productDescription,
+        productImages = productImages
+    )
+}
