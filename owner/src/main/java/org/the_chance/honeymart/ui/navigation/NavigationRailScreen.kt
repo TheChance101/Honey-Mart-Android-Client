cd3c22b07dd7fe71569c05f39ac55f9ef30eec77
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-06 03:01:11 +0300

    Owner| implement NavigationRail with navigation

---
+++ owner/src/main/java/org/the_chance/honeymart/ui/navigation/NavigationRailScreen.kt
@@ -0,0 +1,21 @@
+package org.the_chance.honeymart.ui.navigation
+
+import org.the_chance.design_system.R
+
+sealed class NavigationRailScreen(
+    val route: String,
+    val label: String,
+    val selectedIcon: Int,
+) {
+    object Orders : NavigationRailScreen(
+        route = Screen.Orders.route,
+        label = "Orders",
+        selectedIcon = R.drawable.ic_orders,
+    )
+
+    object Category : NavigationRailScreen(
+        route = Screen.Category.route,
+        label = "Category",
+        selectedIcon = R.drawable.ic_category,
+    )
+}
