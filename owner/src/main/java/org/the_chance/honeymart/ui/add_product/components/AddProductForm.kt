e95bcaaabbb25417801932e49e0b38a10657a075
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-08-06 22:35:42 +0300

    Owner | update loading box

--- owner/src/main/java/org/the_chance/honeymart/ui/add_product/components/AddProductForm.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/add_product/components/AddProductForm.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -125,10 +126,15 @@ fun AddProductForm(
             }
         }
         Spacer(modifier = Modifier.weight(1F))
-        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(MaterialTheme.dimens.space48),
+            contentAlignment = Alignment.Center
+        ) {
             Loading(
                 state = state.isLoading,
-                modifier = Modifier.size(MaterialTheme.dimens.icon48)
+                modifier = Modifier.size(MaterialTheme.dimens.smallLottieLoading)
             )
         }
         HoneyFilledIconButton(
