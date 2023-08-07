7a453058cfd256b7bee6549ed919f37de50a1d3e
Aziza <aziza.helmy35@gmail.com>
2023-08-07 21:28:29 +0300

    Owner | init commit to add CategoryScreen

---
+++ owner/src/main/java/org/the_chance/honeymart/ui/category/categories/CategoriesViewModel.kt
@@ -0,0 +1,54 @@
+package org.the_chance.honeymart.ui.category.categories
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import org.the_chance.honeymart.domain.usecase.GetAllCategoriesInMarketUseCase
+import org.the_chance.honeymart.domain.util.ErrorHandler
+import org.the_chance.honeymart.ui.base.BaseViewModel
+import javax.inject.Inject
+
+/**
+ * Created by Aziza Helmy on 8/7/2023.
+ */
+@HiltViewModel
+class CategoriesViewModel @Inject constructor(
+    private val getAllCategories: GetAllCategoriesInMarketUseCase
+) : BaseViewModel<CategoriesUiState, CategoriesUiEffect>(CategoriesUiState()),
+    CategoriesInteractionsListener {
+
+
+    override val TAG: String
+        get() = TODO("Not yet implemented")
+
+    private fun getAllCategory() {
+        _state.update { it.copy(isLoading = true, isError = false) }
+        tryToExecute(
+            { getAllCategories(1).map { it.toCategoryUiState() } },
+            ::onGetCategorySuccess,
+            ::onGetCategoryError
+        )
+    }
+
+    private fun onGetCategorySuccess(categories: List<CategoryUiState>) {
+        this._state.update {
+            it.copy(
+                isLoading = false,
+                error = null,
+                categories = categories,
+            )
+        }
+    }
+
+    private fun onGetCategoryError(error: ErrorHandler) {
+        _state.update { it.copy(isLoading = false, error = error) }
+        if (error is ErrorHandler.NoConnection) {
+            _state.update { it.copy(isError = true) }
+        }
+    }
+
+    override fun onClickCategory(categoryId: Long, position: Int) {
+
+
+    }
+
+}
