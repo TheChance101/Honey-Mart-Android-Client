6692d75bef53b34fc7091472d56df46fc1fc2700
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-08-07 13:35:16 +0300

    fix snack bar Compose to show in duration time

--- design_system/src/main/java/org/the_chance/honymart/ui/composables/SnackBar.kt
+++ design_system/src/main/java/org/the_chance/honymart/ui/composables/SnackBar.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Snackbar
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.constraintlayout.compose.ConstraintLayout
 import org.the_chance.honymart.ui.theme.Typography
@@ -36,8 +41,61 @@ fun SnackBar(
                 action = {
                     Text(
                         "Undo",
-                        modifier = Modifier.clickable(onClick = onHideSnackbar),
-                        style = Typography.bodySmall,
+                        modifier = Modifier.clickable(onClick = {
+                            onHideSnackbar()
+
+                        }),
+                        style = Typography.displayLarge,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                },
+            ) {
+                Text(
+                    text = message,
+                    style = Typography.bodySmall,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SnackBarWithDuration(
+    message: String,
+    onHideSnackbar: () -> Unit,
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val snackBar = createRef()
+        LaunchedEffect(key1 = snackbarHostState) {
+            snackbarHostState.showSnackbar(
+                message = message,
+                actionLabel = "Close",
+                duration = SnackbarDuration.Long,
+            )
+        }
+
+
+        SnackbarHost(
+            hostState = snackbarHostState,
+            modifier = Modifier.constrainAs(snackBar) {
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+        ) {
+            Snackbar(
+                modifier = Modifier.padding(horizontal = MaterialTheme.dimens.space16),
+                action = {
+                    Text(
+                        "Undo",
+                        modifier = Modifier.clickable(onClick = {
+                            onHideSnackbar()
+                            snackbarHostState.currentSnackbarData?.dismiss()
+                        }),
+                        style = Typography.displayLarge,
                         color = MaterialTheme.colorScheme.primary
                     )
                 },
