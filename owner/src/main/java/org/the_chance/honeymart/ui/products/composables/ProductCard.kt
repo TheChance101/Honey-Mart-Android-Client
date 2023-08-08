ac66a010602246684f761098b9de09df13e6d47b
Narges-Nagy <nargesnagy21@gmail.com>
2023-08-09 00:30:43 +0300

    modify design for product card

--- owner/src/main/java/org/the_chance/honeymart/ui/products/composables/ProductCard.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/products/composables/ProductCard.kt
@@ -1,24 +1,22 @@
 package org.the_chance.honeymart.ui.products.composables
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.the_chance.design_system.R
 import org.the_chance.honymart.ui.composables.ImageNetwork
 import org.the_chance.honymart.ui.theme.HoneyMartTheme
+import org.the_chance.honymart.ui.theme.black37
 import org.the_chance.honymart.ui.theme.blackOn60
 import org.the_chance.honymart.ui.theme.dimens
 
@@ -27,6 +25,7 @@ fun ProductCard(
     imageUrl: String,
     productName: String,
     productPrice: String,
+    numberOfItems: String,
     modifier: Modifier = Modifier
 ) {
     Row(
@@ -40,30 +39,53 @@ fun ProductCard(
                 .clip(CircleShape),
             imageUrl = imageUrl
         )
-        Box(
-            modifier = Modifier.wrapContentSize()
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = MaterialTheme.dimens.space12,
-                        bottom = MaterialTheme.dimens.space12
-                    )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
+                Column(
+                    modifier = Modifier
+                        .padding(
+                            top = MaterialTheme.dimens.space12,
+                            bottom = MaterialTheme.dimens.space12
+                        )
+                ) {
+                    Text(
+                        text = productName,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = blackOn60
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            modifier = Modifier.size(20.dp),
+                            painter = painterResource(id = R.drawable.icon_cart),
+                            contentDescription = "cart icon",
+                            tint = black37
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text(
+                            text = numberOfItems,
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = black37
+                        )
+                    }
+
+                }
                 Text(
-                    text = productName,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = blackOn60
-                )
-                Text(
-                    modifier = Modifier.align(Alignment.End),
                     text = productPrice,
                     style = MaterialTheme.typography.bodyMedium,
                     color = blackOn60
                 )
             }
-        }
+
+
+       // }
     }
 }
 
@@ -75,6 +97,7 @@ fun PreviewProductCard() {
             imageUrl = "",
             productName = "Fresh fruits",
             productPrice = "30,000$",
+            numberOfItems = "3"
         )
     }
 }
