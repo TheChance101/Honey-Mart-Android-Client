5f492969fd338208f36e9b8fa15e2740b8ece1a4
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-08-08 22:54:25 +0300

    Owner | add AddProductWithImagesUseCase

---
+++ core/domain/src/main/java/org/the_chance/honeymart/domain/usecase/AddProductWithImagesUseCase.kt
@@ -0,0 +1,21 @@
+package org.the_chance.honeymart.domain.usecase
+
+import org.the_chance.honeymart.domain.model.ProductEntity
+import org.the_chance.honeymart.domain.repository.HoneyMartRepository
+import javax.inject.Inject
+
+class AddProductWithImagesUseCase @Inject constructor(
+    private val honeyMartRepository: HoneyMartRepository
+) {
+    suspend operator fun invoke(
+        name: String,
+        price: Double,
+        description: String,
+        categoriesId: Long,
+        images: List<String>
+    ): ProductEntity {
+        val product = honeyMartRepository.addProduct(name, price, description, categoriesId)
+        honeyMartRepository.addImageProduct(product.productId, images)
+        return product
+    }
+}
