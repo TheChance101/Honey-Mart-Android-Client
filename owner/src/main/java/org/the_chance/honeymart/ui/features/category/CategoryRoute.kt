f31798305c20fd2822cc7a0066ccc7b6b77e46be
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-11 19:23:49 +0300

    owner| refactor categoryRoute

--- owner/src/main/java/org/the_chance/honeymart/ui/features/category/CategoryRoute.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/category/CategoryRoute.kt
@@ -3,6 +3,7 @@ package org.the_chance.honeymart.ui.features.category
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import org.the_chance.honeymart.ui.category.CategoriesScreen
 import org.the_chance.honeymart.ui.navigation.Screen
 
 private val ROUTE = Screen.Category.route
@@ -15,6 +16,6 @@ fun NavGraphBuilder.categoryRoute() {
     composable(
         route = ROUTE,
     ) {
-        CategoryScreen()
+        CategoriesScreen()
     }
 }
