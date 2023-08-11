c2a5098231c982fb8c9898ce947d510da2d49fdf
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-11 19:21:12 +0300

    owner| add signupRoute

--- owner/src/main/java/org/the_chance/honeymart/ui/features/signup/SignupRoute.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/signup/SignupRoute.kt
@@ -2,13 +2,15 @@ package org.the_chance.honeymart.ui.features.signup
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import org.the_chance.honeymart.ui.navigation.Screen
 
-//private val ROUTE = Screen.SignupScreen.route
+private val ROUTE = Screen.Signup.route
 
 fun NavController.navigateToSignupScreen() {
-//    navigate(ROUTE)
+    navigate(ROUTE)
 }
 
 fun NavGraphBuilder.signupRoute() {
-//    composable(ROUTE) { SignupScreen() }
+    composable(ROUTE) { SignupScreen() }
 }
