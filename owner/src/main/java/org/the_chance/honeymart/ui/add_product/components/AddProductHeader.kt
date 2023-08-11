7b2aaf638f0eee781feba98a76fd67e607146c14
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-08-12 01:46:51 +0300

    Owner | remove unused imports

--- owner/src/main/java/org/the_chance/honeymart/ui/add_product/components/AddProductHeader.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/add_product/components/AddProductHeader.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -16,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
