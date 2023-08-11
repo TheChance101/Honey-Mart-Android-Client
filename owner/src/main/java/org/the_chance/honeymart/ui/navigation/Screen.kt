51ccf77e0dbd305555134db8298cd8b0a6ec379a
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-11 19:21:39 +0300

    owner| refactor screen

--- owner/src/main/java/org/the_chance/honeymart/ui/navigation/Screen.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/navigation/Screen.kt
@@ -3,4 +3,6 @@ package org.the_chance.honeymart.ui.navigation
 sealed class Screen(val route: String) {
     object Orders : Screen("Orders")
     object Category : Screen("Category")
+    object Login : Screen("Login")
+    object Signup : Screen("Signup")
 }
