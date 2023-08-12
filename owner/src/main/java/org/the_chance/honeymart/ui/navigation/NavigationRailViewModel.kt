de0db21ef6adabb7f15b07a14656a60b025a9e12
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-08-12 15:09:14 +0300

    create NavigationViewModel

--- owner/src/main/java/org/the_chance/honeymart/ui/navigation/NavigationRailViewModel.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/navigation/NavigationRailViewModel.kt
@@ -1,10 +1,22 @@
 package org.the_chance.honeymart.ui.navigation
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import org.the_chance.honeymart.domain.usecase.GetUserInfoUseCase
 import org.the_chance.honeymart.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class NavigationRailViewModel @Inject constructor(): BaseViewModel< >{
-    
+class NavigationRailViewModel @Inject constructor(
+    private val profileInfo: GetUserInfoUseCase,
+)
+    : BaseViewModel<NavigationRailUiState , NavigationRailEffect>(NavigationRailUiState()){
+    override val TAG: String
+        get() = this::class.simpleName.toString()
+
+
+    private fun getUserInfo() {
+        _state.update { it.copy(
+        ) }
+    }
 }
