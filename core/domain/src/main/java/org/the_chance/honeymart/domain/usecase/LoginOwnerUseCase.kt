de64ba21635177df36446b13c2b0e918587b1e3e
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-15 20:57:42 +0300

    Owner| refactor LoginOwnerUseCase

--- core/domain/src/main/java/org/the_chance/honeymart/domain/usecase/LoginOwnerUseCase.kt
+++ core/domain/src/main/java/org/the_chance/honeymart/domain/usecase/LoginOwnerUseCase.kt
@@ -1,12 +1,27 @@
 package org.the_chance.honeymart.domain.usecase
 
 import org.the_chance.honeymart.domain.repository.AuthRepository
+import org.the_chance.honeymart.domain.util.ValidationState
 import javax.inject.Inject
 
 class LoginOwnerUseCase @Inject constructor(
     private val authRepository: AuthRepository,
+    private val validationLoginFieldsUseCase: ValidationLoginFieldsUseCase,
 ) {
-    suspend operator fun invoke(email: String, password: String): String {
-        return authRepository.loginOwner(email, password)
+    suspend operator fun invoke(email: String, password: String): ValidationState {
+        val emailValidationState = validationLoginFieldsUseCase.validateEmail(email)
+        val passwordValidationState = validationLoginFieldsUseCase.validatePassword(password)
+
+        return if (emailValidationState != ValidationState.VALID_EMAIL) {
+            emailValidationState
+        } else if (passwordValidationState != ValidationState.VALID_PASSWORD) {
+            passwordValidationState
+        } else {
+            val token = authRepository.loginOwner(email, password)
+            authRepository.saveToken(token)
+            authRepository.saveOwnerName("")
+            authRepository.saveOwnerImageUrl("")
+            ValidationState.SUCCESS
+        }
     }
 }
