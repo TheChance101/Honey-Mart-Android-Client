1a6069862b7cb41915450073bdeb7070a48bd85d
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-15 19:54:54 +0300

    Owner| add RootNavigationGraph

---
+++ owner/src/main/java/org/the_chance/honeymart/ui/navigation/RootNavigationGraph.kt
@@ -0,0 +1,24 @@
+package org.the_chance.honeymart.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import org.the_chance.honeymart.LocalNavigationProvider
+
+@Composable
+fun RootNavigationGraph() {
+    val navController = LocalNavigationProvider.current
+
+    NavHost(
+    navController = navController,
+    startDestination = Graph.AUTH_GRAPH
+    )
+    {
+        authNavGraph()
+        mainNavGraph()
+    }
+}
+
+object Graph {
+    const val AUTH_GRAPH = "auth_graph"
+    const val HOME = "main_graph"
+}
