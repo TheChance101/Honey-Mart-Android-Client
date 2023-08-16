4e363ae859f3329ebafb2695dfb73e66bbfc7554
Mennaakram <mennaakram976@gmail.com>
2023-08-16 16:53:58 +0300

    user|add search ui state

--- user/src/main/java/org/the_chance/honeymart/ui/feature/search/SearchUiState.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/search/SearchUiState.kt
@@ -1,5 +1,19 @@
 package org.the_chance.honeymart.ui.feature.search
 
 data class SearchUiState(
-    val Search: String = ""
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val products: List<ProductUiState> = emptyList(),
+    )
+
+data class ProductUiState(
+    val productId: Long = 0L,
+    val productName: String = "",
+    val productPrice: Double = 0.0,
+    val marketName: String = "",
+    val productImages: List<String> = emptyList()
 )
+
+fun SearchUiState.emptySearchPlaceHolder() = this.products.isEmpty() && !this.isError && !this.isLoading
+
+fun SearchUiState.screenContent() = this.products.isNotEmpty() && !this.isError
