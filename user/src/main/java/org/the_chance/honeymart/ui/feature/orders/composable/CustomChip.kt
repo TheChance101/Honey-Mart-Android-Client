8bfcc0a6c5848e0ed9bb5cb8052eceb72a23a264
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-17 16:59:31 +0300

    User| remove unnecessary imports

--- user/src/main/java/org/the_chance/honeymart/ui/feature/orders/composable/CustomChip.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/orders/composable/CustomChip.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -16,7 +15,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Transparent
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.the_chance.honymart.ui.theme.dimens
