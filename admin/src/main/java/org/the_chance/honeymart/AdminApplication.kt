b1fd4c75e9b09bcec2938f8ece3beffa5362aaf0
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-19 18:42:26 +0300

    Admin| add admin application

---
+++ admin/src/main/java/org/the_chance/honeymart/AdminApplication.kt
@@ -0,0 +1,10 @@
+package org.the_chance.honeymart
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Aziza Helmy on 8/4/2023.
+ */
+@HiltAndroidApp
+class AdminApplication : Application()
