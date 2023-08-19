c24494bfe1c79bcbb39980e5cbdd6001565725d7
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-19 18:50:44 +0300

    Admin| remove comment

--- admin/src/main/java/org/the_chance/honeymart/AdminApplication.kt
+++ admin/src/main/java/org/the_chance/honeymart/AdminApplication.kt
@@ -3,8 +3,5 @@ package org.the_chance.honeymart
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-/**
- * Created by Aziza Helmy on 8/4/2023.
- */
 @HiltAndroidApp
 class AdminApplication : Application()
