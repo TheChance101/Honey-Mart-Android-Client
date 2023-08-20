e68d054e40063357952bed06a765edf416500c9f
Mahmoud Akram Al-Mallahi <nalmamoud303@gmail.com>
2023-08-20 22:17:50 +0300

    owner | handle add product button to the appropriate location in the category product screen

--- owner/src/main/java/org/the_chance/honeymart/ui/features/category/composable/CategoryProducts.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/category/composable/CategoryProducts.kt
@@ -18,85 +18,102 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import org.the_chance.design_system.R
 import org.the_chance.honeymart.ui.components.DropDownMenuList
 import org.the_chance.honeymart.ui.composables.EmptyPlaceholder
 import org.the_chance.honeymart.ui.features.category.CategoriesInteractionsListener
 import org.the_chance.honeymart.ui.features.category.CategoriesUiState
+import org.the_chance.honeymart.ui.features.category.CategoriesViewModel
 import org.the_chance.honeymart.ui.features.category.Visibility
 import org.the_chance.honymart.ui.theme.black37
 import org.the_chance.honymart.ui.theme.blackOn60
 import org.the_chance.honymart.ui.theme.dimens
 
 @Composable
+fun CategoryProductsScreen(viewModel: CategoriesViewModel = hiltViewModel()) {
+    CategoryProducts(listener = viewModel)
+}
+
+@Composable
 fun CategoryProducts(
-    state: CategoriesUiState,
+    state: CategoriesUiState = CategoriesUiState(),
     listener: CategoriesInteractionsListener,
 ) {
-    Box() {
-
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.tertiary)
-            .padding(
-                top = MaterialTheme.dimens.space24,
-                start = MaterialTheme.dimens.space16,
-                end = MaterialTheme.dimens.space16,
-                bottom = MaterialTheme.dimens.space16,
-            )
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+    Box(contentAlignment = Alignment.BottomEnd) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.tertiary)
+                .padding(
+                    top = MaterialTheme.dimens.space24,
+                    start = MaterialTheme.dimens.space16,
+                    end = MaterialTheme.dimens.space16,
+                    bottom = MaterialTheme.dimens.space16,
+                )
         ) {
             Row(
-                horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Icon(
-                    modifier = Modifier.size(MaterialTheme.dimens.icon48),
-                    painter = painterResource(id = R.drawable.icon_category),
-                    contentDescription = "category icon",
-                    tint = black37
-                )
-                if (state.categories.isNotEmpty()) {
-                    Text(
-                        text = state.categories[state.position].categoryName,
-                        style = MaterialTheme.typography.bodySmall,
-                        color = blackOn60
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier.size(MaterialTheme.dimens.icon48),
+                        painter = painterResource(id = R.drawable.icon_category),
+                        contentDescription = "category icon",
+                        tint = black37
                     )
-                }
+                    if (state.categories.isNotEmpty()) {
+                        Text(
+                            text = state.categories[state.position].categoryName,
+                            style = MaterialTheme.typography.bodySmall,
+                            color = blackOn60
+                        )
+                    }
 
-            }
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
+                }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
 //                HoneyOutlineText(text = state.productsQuantity)
 
-                DropDownMenuList(
-                    onClickUpdate = { listener.resetShowState(Visibility.UPDATE_CATEGORY) },
-                    onClickDelete = { listener.resetShowState(Visibility.DELETE_CATEGORY) }
-                )
+                    DropDownMenuList(
+                        onClickUpdate = { listener.resetShowState(Visibility.UPDATE_CATEGORY) },
+                        onClickDelete = { listener.resetShowState(Visibility.DELETE_CATEGORY) }
+                    )
+                }
+            }
+            EmptyPlaceholder(state = state.products.isEmpty(), emptyObjectName = "Product")
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16),
+                contentPadding = PaddingValues(vertical = MaterialTheme.dimens.space24)
+            ) {
+                items(state.products.size) { index ->
+                    val product = state.products[index]
+                    ProductCard(
+                        imageUrl = product.productImage,
+                        productName = product.productName,
+                        productPrice = product.productPrice,
+                    )
+                }
             }
         }
-        EmptyPlaceholder(state = state.products.isEmpty(), emptyObjectName = "Product")
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16),
-            contentPadding = PaddingValues(vertical = MaterialTheme.dimens.space24)
-        ) {
-            items(state.products.size) { index ->
-                val product = state.products[index]
-                ProductCard(
-                    imageUrl = product.productImage,
-                    productName = product.productName,
-                    productPrice = product.productPrice,
-                )
-            }
-        }
+
+        AddProductButton(
+            modifier = Modifier
+                .padding(bottom = MaterialTheme.dimens.space48, end = MaterialTheme.dimens.space48),
+            state = true
+        )
     }
+}
 
-
+@Preview(showBackground = true)
+@Composable
+fun categoryProductPreview() {
+    CategoryProductsScreen()
 }
