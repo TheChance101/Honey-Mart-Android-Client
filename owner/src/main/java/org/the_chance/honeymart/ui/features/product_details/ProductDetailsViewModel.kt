88e9e230d5bda60ddb073d758b484f9ba4c3a516
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-08-21 16:53:35 +0300

    get productDetails

--- owner/src/main/java/org/the_chance/honeymart/ui/features/product_details/ProductDetailsViewModel.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/product_details/ProductDetailsViewModel.kt
@@ -1,26 +1,56 @@
 package org.the_chance.honeymart.ui.features.product_details
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import org.the_chance.honeymart.domain.model.ProductEntity
 import org.the_chance.honeymart.domain.usecase.GetProductDetailsUseCase
+import org.the_chance.honeymart.domain.usecase.UpdateProductUseCase
+import org.the_chance.honeymart.domain.util.ErrorHandler
 import org.the_chance.honeymart.ui.base.BaseViewModel
-import org.the_chance.honeymart.ui.features.category.ProductUiState
-import org.the_chance.honeymart.ui.features.products.ProductsInteractionsListener
-import org.the_chance.honeymart.ui.features.products.ProductsUiEffect
-import org.the_chance.honeymart.ui.features.products.ProductsUiState
 import javax.inject.Inject
 
 
 @HiltViewModel
 class ProductDetailsViewModel @Inject constructor(
-    private val productDetailsUseCase: GetProductDetailsUseCase
+    private val productDetailsUseCase: GetProductDetailsUseCase,
+    private val updateProductUseCase: UpdateProductUseCase
 
 ) : BaseViewModel<ProductsDetailsUiState, ProductDetailsUiEffect>(ProductsDetailsUiState()),
     ProductDetailsInteractionListener {
     override val TAG: String
         get() = this::class.simpleName.toString()
 
-    override fun onUpdateProductName(name: String) {
+    init {
+        getData()
+    }
 
+    private fun getData() {
+        getProductDetails(28)
+    }
+
+    private fun getProductDetails(productId: Long) {
+        _state.update { it.copy(isLoading = true, isError = false) }
+        tryToExecute(
+            { productDetailsUseCase(productId) },
+            ::onGetProductDetailsSuccess,
+            ::onGetProductDetailsError
+        )
+    }
+
+    private fun onGetProductDetailsSuccess(productDetails: ProductEntity) {
+        _state.update {
+            it.copy(
+                isLoading = false,
+                products = productDetails.toProductDetailsUiState()
+            )
+        }
+    }
+
+    private fun onGetProductDetailsError(error: ErrorHandler) {
+        _state.update { it.copy(isLoading = false, error = error) }
+    }
+
+    override fun onUpdateProductName(name: String) {
     }
 
     override fun onUpdateProductPrice(price: String) {
@@ -34,5 +64,4 @@ class ProductDetailsViewModel @Inject constructor(
     override fun onUpdateProductImage(uris: List<ByteArray>) {
         TODO("Not yet implemented")
     }
-
 }
