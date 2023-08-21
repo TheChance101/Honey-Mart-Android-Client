d63d830ce570042441600d2f2e84a81ac2877d34
Nour Elden Alaa <nourelden515@gmail.com>
2023-08-21 23:19:12 +0300

    clean the code

--- core/data/src/main/java/org/the_chance/honeymart/data/source/remote/network/AuthInterceptor.kt
+++ core/data/src/main/java/org/the_chance/honeymart/data/source/remote/network/AuthInterceptor.kt
@@ -21,6 +21,20 @@ class AuthInterceptor @Inject constructor(
     override fun intercept(chain: Interceptor.Chain): Response {
         val accessToken = dataStorePreferences.getAccessToken()
         val refreshToken = dataStorePreferences.getRefreshToken()
+        val (oldRequest, oldResponse) = makeRequest(chain, accessToken)
+        if (oldResponse.code == 401 && refreshToken != "" && refreshToken != null) {
+            val response = refreshToken(refreshToken)
+            if (response.code == 200) {
+                retryRequest(response, oldRequest, chain)
+            }
+        }
+        return oldResponse
+    }
+
+    private fun makeRequest(
+        chain: Interceptor.Chain,
+        accessToken: String?
+    ): Pair<Request, Response> {
         val oldRequest = chain
             .request()
             .newBuilder()
@@ -32,41 +46,48 @@ class AuthInterceptor @Inject constructor(
             .build()
 
         val oldResponse = chain.proceed(oldRequest)
-        if (oldResponse.code == 401 && refreshToken != "" && refreshToken != null) {
-            val client = OkHttpClient()
-            val formBody = FormBody.Builder()
-                .add("refreshToken", refreshToken)
-                .build()
-            val newRequest = Request.Builder()
-                .post(formBody)
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .url("${BASE_URL}token/refresh")
-                .build()
-            val response = client.newCall(newRequest).execute()
-            if (response.code == 200) {
-                val jsonData = response.body.string()
-                val gson = Gson()
-                try {
-                    val type = object : TypeToken<BaseResponse<UserLoginDto>>() {}.type
-                    val tokensResponse: BaseResponse<UserLoginDto> = gson.fromJson(jsonData, type)
-                    runBlocking {
-                        val tokens = tokensResponse.value!!
-                        dataStorePreferences.saveTokens(tokens.accessToken!!, tokens.refreshToken!!)
-                        val newAccessToken = tokens.accessToken
-                        val newRequestWithAccessToken = oldRequest.newBuilder()
-                            .removeHeader(AUTHORIZATION)
-                            .addHeader(AUTHORIZATION, "Bearer $newAccessToken")
-                            .build()
-
-                        return@runBlocking chain.proceed(newRequestWithAccessToken)
-                    }
-                } catch (e: Exception) {
-                    Log.e("refreshResponse if", "intercept: ${e.message}")
-                }
-            }
-        }
-        return oldResponse
+        return Pair(oldRequest, oldResponse)
     }
+
+    private fun refreshToken(refreshToken: String): Response {
+        val client = OkHttpClient()
+        val formBody = FormBody.Builder()
+            .add("refreshToken", refreshToken)
+            .build()
+        val newRequest = Request.Builder()
+            .post(formBody)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .url("${BASE_URL}token/refresh")
+            .build()
+        return client.newCall(newRequest).execute()
+    }
+
+    private fun retryRequest(
+        response: Response,
+        oldRequest: Request,
+        chain: Interceptor.Chain
+    ) {
+        val jsonData = response.body.string()
+        val gson = Gson()
+        try {
+            val type = object : TypeToken<BaseResponse<UserLoginDto>>() {}.type
+            val tokensResponse: BaseResponse<UserLoginDto> = gson.fromJson(jsonData, type)
+            runBlocking {
+                val tokens = tokensResponse.value!!
+                dataStorePreferences.saveTokens(tokens.accessToken!!, tokens.refreshToken!!)
+                val newAccessToken = tokens.accessToken
+                val newRequestWithAccessToken = oldRequest.newBuilder()
+                    .removeHeader(AUTHORIZATION)
+                    .addHeader(AUTHORIZATION, "Bearer $newAccessToken")
+                    .build()
+
+                return@runBlocking chain.proceed(newRequestWithAccessToken)
+            }
+        } catch (e: Exception) {
+            Log.e("refreshResponse if", "intercept: ${e.message}")
+        }
+    }
+
 
     companion object {
         private const val API_KEY = "apiKey"
