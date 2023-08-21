0cdbc1b7fe118e3ae3da828017902c3a527bec13
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-08-21 13:46:25 +0300

    update ProductDetailsForm

--- owner/src/main/java/org/the_chance/honeymart/ui/features/product_details/ProductDetailsViewModel.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/product_details/ProductDetailsViewModel.kt
@@ -1,15 +1,38 @@
 package org.the_chance.honeymart.ui.features.product_details
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.the_chance.honeymart.domain.usecase.GetProductDetailsUseCase
 import org.the_chance.honeymart.ui.base.BaseViewModel
 import org.the_chance.honeymart.ui.features.category.ProductUiState
+import org.the_chance.honeymart.ui.features.products.ProductsInteractionsListener
 import org.the_chance.honeymart.ui.features.products.ProductsUiEffect
+import org.the_chance.honeymart.ui.features.products.ProductsUiState
 import javax.inject.Inject
 
 
 @HiltViewModel
 class ProductDetailsViewModel @Inject constructor(
-) : BaseViewModel<ProductUiState, ProductsUiEffect>(ProductUiState()) {
+    private val productDetailsUseCase: GetProductDetailsUseCase
+
+) : BaseViewModel<ProductsDetailsUiState, ProductDetailsUiEffect>(ProductsDetailsUiState()),
+    ProductDetailsInteractionListener {
     override val TAG: String
         get() = this::class.simpleName.toString()
+
+    override fun onUpdateProductName(name: String) {
+
+    }
+
+    override fun onUpdateProductPrice(price: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpdateProductDescription(description: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpdateProductImage(uris: List<ByteArray>) {
+        TODO("Not yet implemented")
+    }
+
 }
