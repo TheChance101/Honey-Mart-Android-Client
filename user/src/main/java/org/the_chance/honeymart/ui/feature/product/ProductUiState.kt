afef92bd50a94c99c54388ae23fc08615634bd02
Nour Elden Alaa <nourelden515@gmail.com>
2023-08-22 03:14:14 +0300

    fix product details

--- user/src/main/java/org/the_chance/honeymart/ui/feature/product/ProductUiState.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/product/ProductUiState.kt
@@ -39,7 +39,7 @@ fun ProductEntity.toProductUiState(): ProductUiState {
         productName = productName,
         productDescription = productDescription,
         productPrice = productPrice,
-        productImages = productImages
+        productImages = productImages.ifEmpty { listOf("","") }
     )
 }
 
