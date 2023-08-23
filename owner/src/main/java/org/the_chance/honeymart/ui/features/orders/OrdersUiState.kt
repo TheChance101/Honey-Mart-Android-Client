961da29026fc4c191733afa4b511c2ba1b1b4dfb
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-08-23 17:32:57 +0300

    Owner | add toDateFormat and toPriceFormat as extensions to ui state

--- owner/src/main/java/org/the_chance/honeymart/ui/features/orders/OrdersUiState.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/orders/OrdersUiState.kt
@@ -1,9 +1,12 @@
 package org.the_chance.honeymart.ui.features.orders
 
 import org.the_chance.honeymart.domain.model.MarketOrderEntity
-import org.the_chance.honeymart.domain.model.OrderEntity
 import org.the_chance.honeymart.domain.util.ErrorHandler
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
+//region Ui State
 data class OrdersUiState(
     val isLoading: Boolean = false,
     val isError: Boolean = false,
@@ -15,39 +18,54 @@ data class OrdersUiState(
 
 data class OrderUiState(
     val orderId: Long = 1,
-    val totalPrice: Double = 0.0,
-    val time: Long = 1687259600016,
+    val totalPrice: String = "",
+    val time: String = "",
     val userName: String = "",
     val state: Int = 0,
-    )
-
-fun MarketOrderEntity.toOrderUiState(): OrderUiState {
-    return OrderUiState(
-        orderId = orderId,
-        totalPrice = totalPrice,
-        state = state,
-        time = date,
-        userName = user.fullName
-    )
-}
+)
 
 enum class OrderStates(val state: Int) {
     ALL(0),
     PENDING(1),
     PROCESSING(2),
     DONE(3),
-    CANCELED(5),
+    CANCELED(5)
+}
+// endregion
 
-
-
+// region Mappers
+fun MarketOrderEntity.toOrderUiState(): OrderUiState {
+    return OrderUiState(
+        orderId = orderId,
+        totalPrice = totalPrice.toPriceFormat(),
+        state = state,
+        time = date.toDateFormat(),
+        userName = user.fullName
+    )
 }
 
-fun OrdersUiState.all() = this.orderStates == OrderStates.ALL
-fun OrdersUiState.pending() = this.orderStates == OrderStates.PENDING
-fun OrdersUiState.processing() =this.orderStates == OrderStates.PROCESSING
-fun OrdersUiState.done() =this.orderStates == OrderStates.DONE
-fun OrdersUiState.cancel() =this.orderStates == OrderStates.CANCELED
+fun Long.toDateFormat(): String {
+    val dateFormat = SimpleDateFormat("d MMM hh:mm a", Locale.getDefault())
+    val date = Date(this)
 
-fun OrdersUiState.emptyOrdersPlaceHolder() = orders.isEmpty() && !this.isError && !this.isLoading
+    return dateFormat.format(date)
+}
 
-fun OrdersUiState.screenContent() = this.orders.isNotEmpty() && !this.isError
+fun Double.toPriceFormat(): String = "$this$"
+// endregion
+
+// region Extensions
+fun OrdersUiState.all() = orderStates == OrderStates.ALL
+
+fun OrdersUiState.pending() = orderStates == OrderStates.PENDING
+
+fun OrdersUiState.processing() = orderStates == OrderStates.PROCESSING
+
+fun OrdersUiState.done() = orderStates == OrderStates.DONE
+
+fun OrdersUiState.cancel() = orderStates == OrderStates.CANCELED
+
+fun OrdersUiState.emptyOrdersPlaceHolder() = orders.isEmpty() && !isError && !isLoading
+
+fun OrdersUiState.screenContent() = orders.isNotEmpty() && !isError
+// endregion
