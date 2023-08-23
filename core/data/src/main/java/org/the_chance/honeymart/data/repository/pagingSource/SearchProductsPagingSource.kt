685113e6080c6f81ed1f67039dc607ffb2834a3f
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-24 01:27:32 +0300

    refactor search paging source

--- core/data/src/main/java/org/the_chance/honeymart/data/repository/pagingSource/SearchProductsPagingSource.kt
+++ core/data/src/main/java/org/the_chance/honeymart/data/repository/pagingSource/SearchProductsPagingSource.kt
@@ -8,9 +8,10 @@ import javax.inject.Inject
 class SearchProductsPagingSource @Inject constructor(
     honeyMartService: HoneyMartService,
     private val query: String,
+    private val sortOrder: String,
 ) : BasePagingSource<ProductEntity>(honeyMartService) {
     override suspend fun fetchData(page: Int): List<ProductEntity> {
-        return wrap { honeyMartService.searchForProducts(page = page, query = query) }.value?.map { it.toProductEntity() }
+        return wrap { honeyMartService.searchForProducts(page = page, query = query, sortOrder = sortOrder) }.value?.map { it.toProductEntity() }
             ?: throw Throwable("paging error")
     }
 
