9efe6d67323cd5592cfc5989853b33480893f371
Elboray <elboray456@gmail.com>
2023-08-24 17:00:55 +0300

    owner|refactor OrderStatus button

--- owner/src/main/java/org/the_chance/honeymart/ui/features/orders/composables/OrderStatus.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/orders/composables/OrderStatus.kt
@@ -1,6 +1,9 @@
 package org.the_chance.honeymart.ui.features.orders.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,15 +12,19 @@ import org.the_chance.honymart.ui.composables.HoneyFilledButton
 
 @Composable
 fun OrderStatus() {
-    Row {
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ){
+        Spacer(modifier = Modifier.weight(1F))
         HoneyFilledButton(
             modifier = Modifier.width(146.dp),
-            label = "approve",
+            label = "Approve",
             onClick = {},
         )
         HoneyFilledButton(
             modifier = Modifier.width(146.dp),
-            label = "declined",
+            label = "Declined",
             onClick = {},
         )
     }
