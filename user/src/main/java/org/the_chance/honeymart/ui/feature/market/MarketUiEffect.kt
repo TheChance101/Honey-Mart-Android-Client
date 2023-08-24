b50cc11e1da848e6a3de3a9565839a12a4847454
Abdelrahman Mohamed <Abdelrahmanmohamed82@outlook.com>
2023-08-24 19:55:36 +0300

    delete market screen

--- user/src/main/java/org/the_chance/honeymart/ui/feature/market/MarketUiEffect.kt
+++
@@ -1,7 +0,0 @@
-package org.the_chance.honeymart.ui.feature.market
-
-import org.the_chance.honeymart.ui.base.BaseUiEffect
-
-sealed class MarketUiEffect:BaseUiEffect {
-    data class ClickMarketEffect(val marketId: Long) : MarketUiEffect()
-}
