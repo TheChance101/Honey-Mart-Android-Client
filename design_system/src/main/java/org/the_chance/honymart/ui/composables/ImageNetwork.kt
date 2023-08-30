c1a96925257e0fae1b9e3d4e19a6c247485577db
Abdelrahman Mohamed <Abdelrahmanmohamed82@outlook.com>
2023-08-30 18:52:29 +0300

    new palce holder

--- design_system/src/main/java/org/the_chance/honymart/ui/composables/ImageNetwork.kt
+++ design_system/src/main/java/org/the_chance/honymart/ui/composables/ImageNetwork.kt
@@ -25,9 +25,9 @@ fun ImageNetwork(
             .data(imageUrl)
             .crossfade(true)
             .build(),
-        error = painterResource(R.drawable.no_image),
+        error = painterResource(R.drawable.no_images),
         colorFilter = colorFilter,
-        placeholder = painterResource(R.drawable.no_image),
+        placeholder = painterResource(R.drawable.no_images),
         contentDescription = contentDescription,
         contentScale = contentScale,
         modifier = modifier
