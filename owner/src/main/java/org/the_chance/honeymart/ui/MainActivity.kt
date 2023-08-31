0608c579cafd5ca203e147aa32b5c1e3fde093f8
Mohamed Naser <mahmmedn19@gmail.com>
2023-08-31 10:22:10 +0300

    add splash screen

--- owner/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.the_chance.honeymart.ui.main.MainScreen
@@ -16,6 +17,7 @@ import org.the_chance.honymart.ui.theme.HoneyMartTheme
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             val useDarkIcons = !isSystemInDarkTheme()
             CompositionLocalProvider(LocalNavigationProvider provides rememberNavController()) {
