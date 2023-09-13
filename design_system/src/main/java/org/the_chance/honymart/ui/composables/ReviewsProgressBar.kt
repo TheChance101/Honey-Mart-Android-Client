8db70a0d04bd88ea4000170e6c27f94d430e62d6
Ethaar hussien <ethaar7735@gmail.com>
2023-09-14 00:04:21 +0300

    product reviews

--- design_system/src/main/java/org/the_chance/honymart/ui/composables/ReviewsProgressBar.kt
+++ design_system/src/main/java/org/the_chance/honymart/ui/composables/ReviewsProgressBar.kt
@@ -5,11 +5,13 @@ import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
@@ -18,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -44,15 +47,19 @@ fun ReviewsProgressBar(
         )
     )
 
-
-    Row {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 8.dp)
+    ) {
         Text(text = starNumber, style = Typography.displaySmall)
 
         Icon(
             painter = painterResource(id = R.drawable.star_7),
             contentDescription = "icon star",
             tint = primary100,
-            modifier = Modifier.size(16.dp)
+            modifier = Modifier
+                .size(16.dp)
         )
 
         Box(
@@ -73,9 +80,7 @@ fun ReviewsProgressBar(
                     .animateContentSize()
             )
         }
-
         Text(text = countReview, style = Typography.displaySmall)
-
     }
 
 }
--- user/src/main/java/org/the_chance/honeymart/ui/feature/productreview/ProductReviewsViewModel.kt
+++ user/src/main/java/org/the_chance/honeymart/ui/feature/productreview/ProductReviewsViewModel.kt
@@ -1,16 +1,25 @@
 package org.the_chance.honeymart.ui.feature.productreview
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.paging.PagingData
+import androidx.paging.map
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.update
+import org.the_chance.honeymart.domain.model.Review
 import org.the_chance.honeymart.domain.usecase.GetAllProductReviewsPagingUseCase
+import org.the_chance.honeymart.domain.util.ErrorHandler
 import org.the_chance.honeymart.ui.base.BaseViewModel
 import org.the_chance.honeymart.ui.feature.product_details.ProductDetailsArgs
 import javax.inject.Inject
 
+@HiltViewModel
 class ProductReviewsViewModel @Inject constructor(
     private val productReviewsPagingUseCase: GetAllProductReviewsPagingUseCase,
     savedStateHandle: SavedStateHandle,
 ) : BaseViewModel<ProductReviewsUiState, ProductReviewsUiEffect>(ProductReviewsUiState()),
     ProductReviewsInteractionsListener {
+
     override val TAG: String = this::class.simpleName.toString()
 
     private val args = ProductDetailsArgs(savedStateHandle)
@@ -21,12 +30,35 @@ class ProductReviewsViewModel @Inject constructor(
     }
 
     fun getData() {
-//        getProductReviews(args.productId.toLong())
+        getProductReviews(args.productId.toLong())
     }
 
-//    private fun getProductReviews(productId: Long) {
-//
-//    }
+    private fun getProductReviews(productId: Long) {
+        _state.update { it.copy(isLoading = true, isError = false) }
+        tryToExecutePaging(
+            { productReviewsPagingUseCase(productId) },
+            ::onGetProductReviewsSuccess,
+            ::onGetProductReviewsError
+        )
+    }
+
+
+    private fun onGetProductReviewsSuccess(productId: PagingData<Review>) {
+        val mappedProductReviews = productId.map { it.toProductReviewsUiState() }
+        _state.update {
+            it.copy(
+                isLoading = false,
+                reviews = flowOf(mappedProductReviews)
+            )
+        }
+    }
+
+    private fun onGetProductReviewsError(error: ErrorHandler) {
+        _state.update { it.copy(isLoading = false) }
+        if (error is ErrorHandler.NoConnection) {
+            _state.update { it.copy(isLoading = false, isError = true) }
+        }
+    }
 
     override fun onClickBack() {
         effectActionExecutor(_effect, ProductReviewsUiEffect.OnBackClickEffect)
