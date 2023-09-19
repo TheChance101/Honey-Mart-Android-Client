dcf4faac14e9d4ce0b0202961452cefaebb3197f
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-09-19 23:33:57 +0300

    refactor add review feature

--- buildSrc/src/main/java/DependencyVersions.kt
+++ buildSrc/src/main/java/DependencyVersions.kt
@@ -67,10 +67,6 @@ object DependencyVersions {
     const val PagingRuntimeVersion = "3.1.1"
     const val PagingComposeVersion = "3.2.0"
 
-    //Rating bar
-    const val RATING_BAR = "1.3.4"
-
-
     object ClasspathVersions{
         const val NAVIGATION_CLASSPATH = "2.5.3"
         const val TOOLS_BUILD_CLASSPATH = "4.3.15"
