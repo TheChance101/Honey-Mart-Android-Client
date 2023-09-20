980e660aeac876fbfb6556b11c24a4df3cd639bb
Ethaar hussien <ethaar7735@gmail.com>
2023-09-21 00:04:40 +0300

    owner||  fix reviews

--- owner/src/main/java/org/the_chance/honeymart/ui/features/category/content/ProductDetailsContent.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/category/content/ProductDetailsContent.kt
@@ -1,5 +1,6 @@
 package org.the_chance.honeymart.ui.features.category.content
 
+import android.annotation.SuppressLint
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedVisibility
@@ -22,6 +23,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -53,7 +55,7 @@ import org.the_chance.honymart.ui.composables.ReviewsProgressBar
 import org.the_chance.honymart.ui.theme.Typography
 import org.the_chance.honymart.ui.theme.dimens
 
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ProductDetailsContent(
     titleScreen: String,
@@ -70,115 +72,134 @@ fun ProductDetailsContent(
         contract = ActivityResultContracts.PickMultipleVisualMedia(MAX_IMAGES),
         onResult = { it.handleImageSelection(context, state, listener::onImagesSelected) }
     )
-
-    LazyColumn(
-        modifier = modifier
-            .fillMaxSize()
-            .background(
-                color = MaterialTheme.colorScheme.onTertiary,
-                shape = MaterialTheme.shapes.medium
-            )
-    ) {
-
-        item {
-
-            FormHeader(
-                title = titleScreen,
-                iconPainter = painterResource(id = R.drawable.icon_add_product)
-            )
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16)
-            ) {
-                FormTextField(
-                    text = state.productDetails.productNameState.name,
-                    hint = stringResource(R.string.product_name),
-                    keyboardType = KeyboardType.Text,
-                    onValueChange = listener::onUpdateProductName,
-                    errorMessage = state.productDetails.productNameState.errorState,
-                    isEnable = !state.showScreenState.showProductDetails
-                )
-                FormTextField(
-                    text = state.productDetails.productPriceState.name,
-                    hint = stringResource(R.string.price),
-                    keyboardType = KeyboardType.Number,
-                    onValueChange = listener::onUpdateProductPrice,
-                    errorMessage = state.productDetails.productPriceState.errorState,
-                    isEnable = !state.showScreenState.showProductDetails
-                )
-                FormTextField(
-                    text = state.productDetails.productDescriptionState.name,
-                    hint = stringResource(R.string.description),
-                    keyboardType = KeyboardType.Text,
-                    onValueChange = listener::onUpdateProductDescription,
-                    errorMessage = state.productDetails.productDescriptionState.errorState,
-                    isEnable = !state.showScreenState.showProductDetails
-                )
-            }
-            Text(
-                modifier = Modifier.padding(
-                    top = MaterialTheme.dimens.space24,
-                    start = MaterialTheme.dimens.space16
-                ),
-                text = stringResource(R.string.product_image),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onSecondaryContainer,
-                textAlign = TextAlign.Center,
-            )
-
+    Scaffold(
+        bottomBar = {
             Row(
                 modifier = Modifier
-                    .height(256.dp)
                     .fillMaxWidth()
-                    .padding(MaterialTheme.dimens.space16)
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(vertical = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End),
             ) {
-                if (state.showScreenState.showProductDetails) {
-                    LazyVerticalGrid(
-                        columns = GridCells.Adaptive(MaterialTheme.dimens.card),
-                        verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
-                        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8)
-                    ) {
-                        items(items = state.productDetails.productImage) { image ->
-                            ItemImageProductDetails(image = image)
+                HoneyFilledButton(
+                    modifier = Modifier
+                        .width(146.dp),
+                    label = confirmButton,
+                    onClick = onClickConfirm,
+                    isButtonEnabled = if (state.showProductUpdateContent())
+                        state.showSaveUpdateButton() else true
+                )
+                HoneyOutlineButton(
+                    onClick = onClickCancel,
+                    label = cancelButton,
+                )
+            }
+        },
+        topBar = {
+            FormHeader(
+                title = titleScreen,
+                iconPainter = painterResource(id = R.drawable.icon_add_product),
+                modifier = Modifier.background(MaterialTheme.colorScheme.background)
+            )
+        }
+    ) { PaddingValues ->
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+                .background(
+                    color = MaterialTheme.colorScheme.onTertiary,
+                    shape = MaterialTheme.shapes.medium
+                )
+                .padding(PaddingValues)
+        ) {
+
+            item {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16)
+                ) {
+                    FormTextField(
+                        text = state.productDetails.productNameState.name,
+                        hint = stringResource(R.string.product_name),
+                        keyboardType = KeyboardType.Text,
+                        onValueChange = listener::onUpdateProductName,
+                        errorMessage = state.productDetails.productNameState.errorState,
+                        isEnable = !state.showScreenState.showProductDetails
+                    )
+                    FormTextField(
+                        text = state.productDetails.productPriceState.name,
+                        hint = stringResource(R.string.price),
+                        keyboardType = KeyboardType.Number,
+                        onValueChange = listener::onUpdateProductPrice,
+                        errorMessage = state.productDetails.productPriceState.errorState,
+                        isEnable = !state.showScreenState.showProductDetails
+                    )
+                    FormTextField(
+                        text = state.productDetails.productDescriptionState.name,
+                        hint = stringResource(R.string.description),
+                        keyboardType = KeyboardType.Text,
+                        onValueChange = listener::onUpdateProductDescription,
+                        errorMessage = state.productDetails.productDescriptionState.errorState,
+                        isEnable = !state.showScreenState.showProductDetails
+                    )
+                }
+                Text(
+                    modifier = Modifier.padding(
+                        top = MaterialTheme.dimens.space24,
+                        start = MaterialTheme.dimens.space16
+                    ),
+                    text = stringResource(R.string.product_image),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    textAlign = TextAlign.Center,
+                )
+
+                Row(
+                    modifier = Modifier
+                        .height(150.dp)
+                        .fillMaxWidth()
+                        .padding(MaterialTheme.dimens.space16)
+                ) {
+                    if (state.showScreenState.showProductDetails) {
+                        LazyVerticalGrid(
+                            columns = GridCells.Adaptive(MaterialTheme.dimens.card),
+                            verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
+                            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8)
+                        ) {
+                            items(items = state.productDetails.productImage) { image ->
+                                ItemImageProductDetails(image = image)
+                            }
                         }
-                    }
-                } else if (state.showScreenState.showProductUpdate) {
-                    LazyVerticalGrid(
-                        columns = GridCells.Adaptive(MaterialTheme.dimens.card),
-                        verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
-                        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8)
-                    ) {
-                        items(items = state.newProducts.images) { image ->
-                            ItemImageProduct(image, listener::onClickRemoveSelectedImage)
-                        }
-                        if (state.newProducts.images.size < MAX_IMAGES) {
-                            item {
-                                AddImageButton(multiplePhotoPickerLauncher)
+                    } else if (state.showScreenState.showProductUpdate) {
+                        LazyVerticalGrid(
+                            columns = GridCells.Adaptive(MaterialTheme.dimens.card),
+                            verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
+                            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8)
+                        ) {
+                            items(items = state.newProducts.images) { image ->
+                                ItemImageProduct(image, listener::onClickRemoveSelectedImage)
+                            }
+                            if (state.newProducts.images.size < MAX_IMAGES) {
+                                item {
+                                    AddImageButton(multiplePhotoPickerLauncher)
+                                }
                             }
                         }
                     }
                 }
             }
-        }
-        item {
-            ContentVisibility(state = state.showScreenState.showProductDetails) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp, vertical = 8.dp)
-                        .height(56.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
+            item {
+                ContentVisibility(state = state.showScreenState.showProductDetails) {
                     Text(
-                        modifier = Modifier.padding(bottom = 4.dp),
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
                         text = stringResource(R.string.customers_reviews),
                         style = Typography.bodyMedium,
                         color = MaterialTheme.colorScheme.onSecondary
                     )
                 }
-
-                item {
+            }
+            item {
+                ContentVisibility(state = state.showScreenState.showProductDetails) {
                     AnimatedVisibility(
                         visible = !state.isLoading,
                         enter = fadeIn(animationSpec = tween(durationMillis = 2000)) + slideInVertically(),
@@ -234,11 +255,14 @@ fun ProductDetailsContent(
                         }
                     }
                 }
-                items(state.reviews.reviews.size) { position ->
+            }
+            items(state.reviews.reviews.size) { position ->
 //            onChangeReviews(position)
 //            if ((position + 1) >= (state.page * MAX_PAGE_SIZE)) {
 //                listener.onScrollDown()
 //            }
+
+                ContentVisibility(state = state.showScreenState.showProductDetails) {
                     val review = state.reviews.reviews[position]
                     CardReviews(
                         userName = review.fullName,
@@ -248,27 +272,11 @@ fun ProductDetailsContent(
                     )
                 }
             }
-            item {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 16.dp),
-                    horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End),
-                ) {
-                    HoneyFilledButton(
-                        modifier = Modifier.width(146.dp),
-                        label = confirmButton,
-                        onClick = onClickConfirm,
-                        isButtonEnabled = if (state.showProductUpdateContent())
-                            state.showSaveUpdateButton() else true
-                    )
-                    HoneyOutlineButton(onClick = onClickCancel, label = cancelButton)
-                }
-            }
 
         }
     }
 }
+
 
 
 
