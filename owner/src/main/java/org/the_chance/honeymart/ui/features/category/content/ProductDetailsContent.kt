feaaf8625843547fea13be85ca4cc3ec2bd0cde4
Ethaar hussien <ethaar7735@gmail.com>
2023-09-20 23:05:32 +0300

    owner||  try to fix reviews

--- owner/src/main/java/org/the_chance/honeymart/ui/features/category/content/ProductDetailsContent.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/category/content/ProductDetailsContent.kt
@@ -2,6 +2,12 @@ package org.the_chance.honeymart.ui.features.category.content
 
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,11 +17,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,10 +43,16 @@ import org.the_chance.honeymart.ui.features.category.composable.ItemImageProduct
 import org.the_chance.honeymart.ui.features.category.composable.ItemImageProductDetails
 import org.the_chance.honeymart.ui.features.category.showProductUpdateContent
 import org.the_chance.honeymart.ui.features.category.showSaveUpdateButton
+import org.the_chance.honeymart.ui.util.defaultTo1IfZero
 import org.the_chance.honeymart.ui.util.handleImageSelection
+import org.the_chance.honymart.ui.composables.AverageRating
+import org.the_chance.honymart.ui.composables.CardReviews
 import org.the_chance.honymart.ui.composables.HoneyFilledButton
 import org.the_chance.honymart.ui.composables.HoneyOutlineButton
+import org.the_chance.honymart.ui.composables.ReviewsProgressBar
+import org.the_chance.honymart.ui.theme.Typography
 import org.the_chance.honymart.ui.theme.dimens
+
 
 @Composable
 fun ProductDetailsContent(
@@ -59,113 +70,205 @@ fun ProductDetailsContent(
         contract = ActivityResultContracts.PickMultipleVisualMedia(MAX_IMAGES),
         onResult = { it.handleImageSelection(context, state, listener::onImagesSelected) }
     )
-    Column(
+
+    LazyColumn(
         modifier = modifier
             .fillMaxSize()
             .background(
                 color = MaterialTheme.colorScheme.onTertiary,
                 shape = MaterialTheme.shapes.medium
             )
-            .verticalScroll(rememberScrollState())
     ) {
 
-        FormHeader(
-            title = titleScreen,
-            iconPainter = painterResource(id = R.drawable.icon_add_product)
-        )
+        item {
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16)
-        ) {
-            FormTextField(
-                text = state.productDetails.productNameState.name,
-                hint = stringResource(R.string.product_name),
-                keyboardType = KeyboardType.Text,
-                onValueChange = listener::onUpdateProductName,
-                errorMessage = state.productDetails.productNameState.errorState,
-                isEnable = !state.showScreenState.showProductDetails
+            FormHeader(
+                title = titleScreen,
+                iconPainter = painterResource(id = R.drawable.icon_add_product)
             )
-            FormTextField(
-                text = state.productDetails.productPriceState.name,
-                hint = stringResource(R.string.price),
-                keyboardType = KeyboardType.Number,
-                onValueChange = listener::onUpdateProductPrice,
-                errorMessage = state.productDetails.productPriceState.errorState,
-                isEnable = !state.showScreenState.showProductDetails
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space16)
+            ) {
+                FormTextField(
+                    text = state.productDetails.productNameState.name,
+                    hint = stringResource(R.string.product_name),
+                    keyboardType = KeyboardType.Text,
+                    onValueChange = listener::onUpdateProductName,
+                    errorMessage = state.productDetails.productNameState.errorState,
+                    isEnable = !state.showScreenState.showProductDetails
+                )
+                FormTextField(
+                    text = state.productDetails.productPriceState.name,
+                    hint = stringResource(R.string.price),
+                    keyboardType = KeyboardType.Number,
+                    onValueChange = listener::onUpdateProductPrice,
+                    errorMessage = state.productDetails.productPriceState.errorState,
+                    isEnable = !state.showScreenState.showProductDetails
+                )
+                FormTextField(
+                    text = state.productDetails.productDescriptionState.name,
+                    hint = stringResource(R.string.description),
+                    keyboardType = KeyboardType.Text,
+                    onValueChange = listener::onUpdateProductDescription,
+                    errorMessage = state.productDetails.productDescriptionState.errorState,
+                    isEnable = !state.showScreenState.showProductDetails
+                )
+            }
+            Text(
+                modifier = Modifier.padding(
+                    top = MaterialTheme.dimens.space24,
+                    start = MaterialTheme.dimens.space16
+                ),
+                text = stringResource(R.string.product_image),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                textAlign = TextAlign.Center,
             )
-            FormTextField(
-                text = state.productDetails.productDescriptionState.name,
-                hint = stringResource(R.string.description),
-                keyboardType = KeyboardType.Text,
-                onValueChange = listener::onUpdateProductDescription,
-                errorMessage = state.productDetails.productDescriptionState.errorState,
-                isEnable = !state.showScreenState.showProductDetails
-            )
-        }
-        Text(
-            modifier = Modifier.padding(
-                top = MaterialTheme.dimens.space24,
-                start = MaterialTheme.dimens.space16
-            ),
-            text = stringResource(R.string.product_image),
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onSecondaryContainer,
-            textAlign = TextAlign.Center,
-        )
 
-        Row(
-            modifier = Modifier
-                .height(256.dp)
-                .fillMaxWidth()
-                .padding(MaterialTheme.dimens.space16)
-        ) {
-            if (state.showScreenState.showProductDetails) {
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(MaterialTheme.dimens.card),
-                    verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
-                    horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8)
-                ) {
-                    items(items = state.productDetails.productImage) { image ->
-                        ItemImageProductDetails(image = image)
+            Row(
+                modifier = Modifier
+                    .height(256.dp)
+                    .fillMaxWidth()
+                    .padding(MaterialTheme.dimens.space16)
+            ) {
+                if (state.showScreenState.showProductDetails) {
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(MaterialTheme.dimens.card),
+                        verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
+                        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8)
+                    ) {
+                        items(items = state.productDetails.productImage) { image ->
+                            ItemImageProductDetails(image = image)
+                        }
                     }
-                }
-            } else if (state.showScreenState.showProductUpdate) {
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(MaterialTheme.dimens.card),
-                    verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
-                    horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8)
-                ) {
-                    items(items = state.newProducts.images) { image ->
-                        ItemImageProduct(image, listener::onClickRemoveSelectedImage)
-                    }
-                    if (state.newProducts.images.size < MAX_IMAGES) {
-                        item {
-                            AddImageButton(multiplePhotoPickerLauncher)
+                } else if (state.showScreenState.showProductUpdate) {
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(MaterialTheme.dimens.card),
+                        verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8),
+                        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.space8)
+                    ) {
+                        items(items = state.newProducts.images) { image ->
+                            ItemImageProduct(image, listener::onClickRemoveSelectedImage)
+                        }
+                        if (state.newProducts.images.size < MAX_IMAGES) {
+                            item {
+                                AddImageButton(multiplePhotoPickerLauncher)
+                            }
                         }
                     }
                 }
             }
         }
+        item {
+            ContentVisibility(state = state.showScreenState.showProductDetails) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        .height(56.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        modifier = Modifier.padding(bottom = 4.dp),
+                        text = stringResource(R.string.customers_reviews),
+                        style = Typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSecondary
+                    )
+                }
 
-        ContentVisibility(state = state.showScreenState.showProductDetails) {
-            ProductReviewContent(state = state)
-        }
+                item {
+                    AnimatedVisibility(
+                        visible = !state.isLoading,
+                        enter = fadeIn(animationSpec = tween(durationMillis = 2000)) + slideInVertically(),
+                        exit = fadeOut(animationSpec = tween(durationMillis = 500)) + slideOutHorizontally()
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        ) {
+                            AverageRating(
+                                averageRating = state.reviews.reviewStatisticUiState.averageRating.toFloat(),
+                                reviewCount = state.reviews.reviewStatisticUiState.reviewCount.toString(),
+                            )
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End),
-        ) {
-            HoneyFilledButton(
-                modifier = Modifier.width(146.dp),
-                label = confirmButton,
-                onClick = onClickConfirm,
-                isButtonEnabled = if (state.showProductUpdateContent())
-                    state.showSaveUpdateButton() else true
-            )
-            HoneyOutlineButton(onClick = onClickCancel, label = cancelButton)
+                            ReviewsProgressBar(
+                                starNumber = "5",
+                                countReview = state.reviews.reviewStatisticUiState.fiveStarsCount.toString(),
+                                rating =
+                                (state.reviews.reviewStatisticUiState.fiveStarsCount.toFloat() /
+                                        state.reviews.reviewStatisticUiState.reviewCount.defaultTo1IfZero()
+                                        )
+                            )
+
+                            ReviewsProgressBar(
+                                starNumber = "4",
+                                countReview = state.reviews.reviewStatisticUiState.fourStarsCount.toString(),
+                                rating =
+                                (state.reviews.reviewStatisticUiState.fourStarsCount.toFloat() /
+                                        state.reviews.reviewStatisticUiState.reviewCount
+                                            .defaultTo1IfZero())
+                            )
+                            ReviewsProgressBar(
+                                starNumber = "3",
+                                countReview = state.reviews.reviewStatisticUiState.threeStarsCount.toString(),
+                                rating = (state.reviews.reviewStatisticUiState.threeStarsCount.toFloat() /
+                                        state.reviews.reviewStatisticUiState.reviewCount
+                                            .defaultTo1IfZero())
+                            )
+                            ReviewsProgressBar(
+                                starNumber = "2",
+                                countReview = state.reviews.reviewStatisticUiState.twoStarsCount.toString(),
+                                rating = (state.reviews.reviewStatisticUiState.twoStarsCount.toFloat() /
+                                        state.reviews.reviewStatisticUiState.reviewCount
+                                            .defaultTo1IfZero())
+                            )
+                            ReviewsProgressBar(
+                                starNumber = "1",
+                                countReview = state.reviews.reviewStatisticUiState.oneStarCount.toString(),
+                                rating = (state.reviews.reviewStatisticUiState.oneStarCount.toFloat() /
+                                        state.reviews.reviewStatisticUiState.reviewCount
+                                            .defaultTo1IfZero())
+                            )
+                        }
+                    }
+                }
+                items(state.reviews.reviews.size) { position ->
+//            onChangeReviews(position)
+//            if ((position + 1) >= (state.page * MAX_PAGE_SIZE)) {
+//                listener.onScrollDown()
+//            }
+                    val review = state.reviews.reviews[position]
+                    CardReviews(
+                        userName = review.fullName,
+                        rating = review.rating.toFloat(),
+                        reviews = review.content,
+                        date = review.reviewDate
+                    )
+                }
+            }
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End),
+                ) {
+                    HoneyFilledButton(
+                        modifier = Modifier.width(146.dp),
+                        label = confirmButton,
+                        onClick = onClickConfirm,
+                        isButtonEnabled = if (state.showProductUpdateContent())
+                            state.showSaveUpdateButton() else true
+                    )
+                    HoneyOutlineButton(onClick = onClickCancel, label = cancelButton)
+                }
+            }
+
         }
     }
 }
+
+
+
