b8ec4e251e244b1a51e73186b0004da6b459abca
Ethaar hussien <ethaar7735@gmail.com>
2023-09-20 14:50:01 +0300

    owner|| ui state reviews

--- owner/src/main/java/org/the_chance/honeymart/ui/features/category/CategoriesUiState.kt
+++ owner/src/main/java/org/the_chance/honeymart/ui/features/category/CategoriesUiState.kt
@@ -1,9 +1,15 @@
 package org.the_chance.honeymart.ui.features.category
 
+import android.annotation.SuppressLint
 import org.the_chance.honeymart.domain.model.Category
 import org.the_chance.honeymart.domain.model.Product
+import org.the_chance.honeymart.domain.model.ProductRating
+import org.the_chance.honeymart.domain.model.ProductReview
+import org.the_chance.honeymart.domain.model.Reviews
 import org.the_chance.honeymart.domain.util.ErrorHandler
 import org.the_chance.honeymart.ui.util.toPriceFormat
+import java.text.SimpleDateFormat
+import java.util.Date
 
 /**
  * Created by Aziza Helmy on 8/7/2023.
@@ -209,3 +215,69 @@ fun CategoriesUiState.showCategoryProductsInCategory() =
 fun CategoriesUiState.showLoadingWhenCategoriesIsEmpty() = isLoading && categories.isEmpty()
 
 // endregion
+
+data class ProductReviewsUiState(
+    val isLoading: Boolean = true,
+    val isError: Boolean = false,
+    val error: ErrorHandler? = null,
+    val page: Int = 1,
+    val reviews: ReviewDetailsUiState = ReviewDetailsUiState(),
+)
+
+data class ReviewDetailsUiState(
+    val reviewStatisticUiState: ProductRatingUiState = ProductRatingUiState(),
+    val reviews: List<ProductReviewUiState> = listOf()
+)
+
+fun Reviews.toReviews(): ReviewDetailsUiState {
+    return ReviewDetailsUiState(
+        reviewStatisticUiState = reviewStatistic.toReviewStatisticUiState(),
+        reviews = reviews.map { it.toProductReviewUiState() }
+    )
+}
+
+data class ProductRatingUiState(
+    val averageRating: Double = 0.0,
+    val reviewCount: Int = 0,
+    val oneStarCount: Int = 0,
+    val twoStarsCount: Int = 0,
+    val threeStarsCount: Int = 0,
+    val fourStarsCount: Int = 0,
+    val fiveStarsCount: Int = 0,
+)
+
+fun ProductRating.toReviewStatisticUiState(): ProductRatingUiState {
+    return ProductRatingUiState(
+        averageRating = averageRating,
+        reviewCount = reviewsCount,
+        oneStarCount = oneStarCount,
+        twoStarsCount = twoStarsCount,
+        threeStarsCount = threeStarsCount,
+        fourStarsCount = fourStarsCount,
+        fiveStarsCount = fiveStarsCount
+    )
+}
+
+data class ProductReviewUiState(
+    val reviewId: Long,
+    val content: String,
+    val rating: Int,
+    val reviewDate: String,
+    val fullName: String,
+)
+
+fun ProductReview.toProductReviewUiState(): ProductReviewUiState {
+    return ProductReviewUiState(
+        reviewId = reviewId,
+        content = content,
+        rating = rating,
+        reviewDate = reviewDate.toDateFormat(),
+        fullName = user.fullName
+    )
+}
+
+@SuppressLint("SimpleDateFormat")
+fun Date.toDateFormat(): String {
+    val dateFormat = SimpleDateFormat("dd MMM  HH:mm")
+    return dateFormat.format(this)
+}
